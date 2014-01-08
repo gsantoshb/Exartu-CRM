480beab905deefd6287e8f9b801e69baf711c4c3
jberneche <javier@ubuntu.(none)>
2014-01-08 16:58:56 -0200

    add users from google fixed

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,14 @@
 Accounts.onCreateUser(function(options, user) {
     console.dir(options);
     console.dir(user);
+    if(user.services){
+        if(user.services.google){
+            //todo: check if the account is allready in the database
+            var email=user.services.google.email;
+            options.hierId=createHier(email.split('@')[0]);
+            user.emails=[{"address": email, "verified": true}];
+        }
+    }
 	if (options.hierId == null) {
 		options.hierId = createHier(options.email.split('@')[0]);
 	}
