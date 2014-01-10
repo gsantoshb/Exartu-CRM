c6d9ca17e8caa68ceb3c0d66ae9a359bea7b2ac2
psoto <psoto@ubuntu.(none)>
2014-01-10 16:22:54 -0600

    Contactable's tags merge fix

--- client/js/contactable.js
+++ client/js/contactable.js
@@ -25,6 +25,31 @@ ContactableController = RouteController.extend({
 		// save current contactable to later use on templates
 		Session.set('contactableId', this.params._id);
 	},
+    layoutTemplate: 'contactableLayout',
+    action: function () {
+        // define which template to render in function of the url's hash
+        switch (this.params.hash) {
+        case 'messages':
+            this.render('test');
+            break;
+        case 'activities':
+            this.render('activities');
+            break;
+        case undefined:
+            this.render('activities');
+            break;
+        };
+        // render contactableNavegation template on navegation regin defined on contactableLayout (client/layouts.html)
+        this.render('contactableNavigation', {
+            to: 'navegation'
+        });
+        this.render('tags', {
+            to: 'information'
+        });
+    },
+    data: function () {
+        Session.set('contactableId', this.params._id); // save current contactable to later use on templates
+    },
 });
 
 Template.contactableLayout.rendered = function () {
