2d3fd5a53ad8e57eb4da54c8184f9bc9c5910e83
psoto <psoto@ubuntu.(none)>
2014-01-13 15:59:20 -0600

    Add message from activities list

--- client/js/entityMessages.js
+++ client/js/entityMessages.js
@@ -1,18 +1,11 @@
 Template.entityMessages.rendered = function () {
 	var vm = function () {
 		var self = this,
-			entityId = Session.get('entityId'),
-			entityCollection = Session.get('entityCollection');
+			entityId = Session.get('entityId');
 
 		self.messages = ko.meteor.find(Messages, {
 			entityIds: entityId
 		});
-
-		self.addMessage = function () {
-			var messageId = Meteor.call('createMessage', {
-				message: "msg test2",
-			}, [entityId]);
-		};
 
 		self.editMessage = function (data, index) {
 
@@ -23,9 +16,3 @@ Template.entityMessages.rendered = function () {
 
 	ko.applyBindings(new vm(), document.getElementsByName('entityMessagesVM')[0]);
 };
-
-Meteor.methods({
-	createMessage: function (message, entityList) {
-		Messages.insert(message);
-	}
-});
