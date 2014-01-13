c7c2e1b3b30ecf1d8cf5c300f082987826b995f0
jberneche <javier@ubuntu.(none)>
2014-01-13 12:18:46 -0200

    add contactable and contactables list changed, using the second parameter of apply bindings

--- client/js/contactables.js
+++ client/js/contactables.js
@@ -8,7 +8,6 @@ var contactablesHandle = Meteor.subscribe('contactables');
 Template.contactables.rendered = function () {
     var viewModel = function () {
         var self = this;
-        self.addVM = addVM();
         self.filter = ko.observable({});
         self.entities = ko.meteor.find(Contactables, {});
         self.filter = ko.observableArray([{
@@ -40,48 +39,5 @@ Template.contactables.rendered = function () {
         })
     };
 
-    ko.applyBindings(new viewModel());
-};
-
-var addVM = function () {
-    var viewModel = function () {
-        var self = this;
-        self.firstName = ko.observable('');
-        self.lastName = ko.observable('');
-        self.organizationName = ko.observable('');
-        self.department = ko.observable('');
-        self.statusNote = ko.observable('');
-        self.isEmployee = ko.observable(true);
-        self.isContact = ko.observable(false);
-        self.isCustomer = ko.observable(false);
-        self.person = ko.computed(function () {
-            return self.isEmployee() || self.isContact();
-        });
-        self.organization = ko.computed(function () {
-            return self.isCustomer();
-        });
-        self.addContactable = function () {
-            var newContactable = {
-                isEmployee: self.isEmployee(),
-                isContact: self.isContact(),
-                isCustomer: self.isCustomer(),
-                statusNote: self.statusNote(),
-            }
-            if (!newContactable.isContact && !newContactable.isCustomer && !newContactable.isEmployee) reuturn;
-
-            if (newContactable.isContact || newContactable.isEmployee) {
-                newContactable.firstName = self.firstName();
-                newContactable.lastName = self.lastName();
-            }
-            if (newContactable.isCustomer) {
-                newContactable.organizationName = self.organizationName();
-                newContactable.department = self.department();
-            }
-            Meteor.call('addContactable', newContactable);
-            $('#addContactableModal').modal('hide');
-        }
-        return this;
-    }
-
-    return new viewModel();
+    ko.applyBindings(new viewModel(), document.getElementsByName('contactablesVM')[0]);
 };
