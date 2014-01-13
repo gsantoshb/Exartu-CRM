98c38984f7d714ed59f5312aba89bfae06527caf
psoto <psoto@ubuntu.(none)>
2014-01-13 15:59:20 -0600

    Add message from activities list

---
+++ client/js/activities.js
@@ -0,0 +1,23 @@
+Template.activities.rendered = function () {
+	var vm = function () {
+		var self = this,
+			entityId = Session.get('entityId');
+
+		self.newMessage = ko.observable();
+		self.addMessage = function () {
+			var messageId = Meteor.call('createMessage', {
+				message: self.newMessage(),
+			}, [entityId]);
+		};
+
+		return self;
+	};
+
+	ko.applyBindings(new vm(), document.getElementsByName('activitiesVM')[0]);
+};
+
+Meteor.methods({
+	createMessage: function (message, entityList) {
+		Messages.insert(message);
+	}
+});
