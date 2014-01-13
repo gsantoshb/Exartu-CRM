d5b414d766e65dd27f8c9c1c96c94950a1617894
jberneche <javier@ubuntu.(none)>
2014-01-13 11:51:02 -0200

    remove add contactable's modal  from index

--- client/js/contactable.js
+++ client/js/contactable.js
@@ -9,13 +9,16 @@ ContactableController = RouteController.extend({
         case 'activities':
             this.render('activities');
             break;
+        case 'asendEmail':
+            this.render('sendEmail');
+            break;                
         case undefined:
             this.render('activities');
             break;
         };
-        // render contactableNavegation template on navegation regin defined on contactableLayout (client/layouts.html)
+        // render contactableNavigation template on navigation region defined on contactableLayout (client/layouts.html)
         this.render('contactableNavigation', {
-            to: 'navegation'
+            to: 'navigation'
         });
         this.render('tags', {
             to: 'information'
@@ -25,51 +28,54 @@ ContactableController = RouteController.extend({
         Session.set('contactableId', this.params._id); // save current contactable to later use on templates
     },
 });
+
 Template.contactableLayout.rendered = function () {
-    var viewModel = function () {
-        var self = this;
-        self.navigationVM = getNavigationVM();
-        self.tagVM = getTagVM();
-    }
-    ko.applyBindings(new viewModel());
+	var viewModel = function () {
+		var self = this;
+		self.navigationVM = getNavigationVM();
+		self.tagVM = getTagVM();
+		self.messageVM = getEntityMessageVM(Session.get('contactableId'));
+	}
+
+	ko.applyBindings(new viewModel());
 };
 
 var getNavigationVM = function () {
-    // load contactable information
-    var contactableVM = function () {
-        var self = this;
-        self.contactable = ko.meteor.findOne(Contactables, {
-            _id: Session.get('contactableId')
-        });
-        self.contactable().displayName = ko.computed(
-            function () {
-                return self.contactable().firstName() + ', ' + self.contactable().lastName();
-            }, self);
+	// load contactable information
+	var contactableVM = function () {
+		var self = this;
+		self.contactable = ko.meteor.findOne(Contactables, {
+			_id: Session.get('contactableId')
+		});
+		self.contactable().displayName = ko.computed(
+			function () {
+				return self.contactable().firstName() + ', ' + self.contactable().lastName();
+			}, self);
 
-        return self;
-    };
-    return new contactableVM()
+		return self;
+	};
+	return new contactableVM()
 };
 
 var getTagVM = function () {
-    var viewModel = function () {
-        var self = this;
-        self.contactable = ko.meteor.findOne(Contactables, {
-            _id: Session.get('contactableId')
-        });
-        self.tags = self.contactable().tags;
-        self.newTag = ko.observable('');
-        self.isAdding = ko.observable(false);
-        self.addTag = function () {
-            Contactables.update({
-                _id: Session.get('contactableId')
-            }, {
-                $addToSet: {
-                    tags: self.newTag()
-                }
-            })
-        }
-        return self;
-    };
-    return new viewModel();
+	var viewModel = function () {
+		var self = this;
+		self.contactable = ko.meteor.findOne(Contactables, {
+			_id: Session.get('contactableId')
+		});
+		self.tags = self.contactable().tags;
+		self.newTag = ko.observable('');
+		self.isAdding = ko.observable(false);
+		self.addTag = function () {
+			Contactables.update({
+				_id: Session.get('contactableId')
+			}, {
+				$addToSet: {
+					tags: self.newTag()
+				}
+			})
+		}
+		return self;
+	};
+	return new viewModel();
 }
--- server/collections/hierarchy.js
+++ server/collections/hierarchy.js
@@ -3,17 +3,18 @@ Hierarchies = new Meteor.Collection("hierarchies");
 createHier = function(name, parent) {
 	return Meteor.call('createHier', { name: name, parent: parent });
 }
+Meteor.startup(function() {
+    Meteor.methods({
+        createHier: function(options) {
+            //TODO: Check options values
 
-Meteor.methods({
-	createHier: function(options) {
-		//TODO: Check options values
-		
-		if (options.parent != null)	{
-			var parentHier = Hierarchies.findOne({_id: options.parent}).fetch();
-			if (parentHier == null)
-				throw new Meteor.Error(404, "Parent hierarchy does not exist");
-		}
-		
-		return Hierarchies.insert({name: options.name, parent: options.parent});
-	},
+            if (options.parent != null)	{
+                var parentHier = Hierarchies.findOne({_id: options.parent}).fetch();
+                if (parentHier == null)
+                    throw new Meteor.Error(404, "Parent hierarchy does not exist");
+            }
+
+            return Hierarchies.insert({name: options.name, parent: options.parent});
+        }
+    });
 });
