7819c9391872c2616453db27011d2979c5020904
jberneche <javier@ubuntu.(none)>
2014-01-13 11:24:11 -0200

    add contactables with knockout

--- server/collections/contactable.js
+++ server/collections/contactable.js
@@ -1,26 +1,31 @@
 Contactables = new Meteor.Collection("contactables");
 
 Meteor.publish('contactables', function () {
-	var user = Meteor.users.findOne({
-		_id: this.userId
-	});
+    var user = Meteor.users.findOne({
+        _id: this.userId
+    });
 
-	if (!user)
-		return false;
+    if (!user)
+        return false;
 
-	return Contactables.find({
-		hierId: user.hierId
-	});
+    return Contactables.find({
+        hierId: user.hierId
+    });
 })
 
 Meteor.methods({
-	addContactable: function (contactable) {
-		var user = Meteor.user();
-		if (user == null)
-			throw new Meteor.Error(401, "Please login");
+    addContactable: function (contactable) {
+        var user = Meteor.user();
+        if (user == null)
+            throw new Meteor.Error(401, "Please login");
 
-		addSystemMetadata(contactable, user);
+        addSystemMetadata(contactable, user);
 
-		Contactables.insert(contactable);
-	}
+        Contactables.insert(contactable);
+    }
 });
+Contactables.allow({
+    update: function () {
+        return true;
+    }
+})
