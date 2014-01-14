b2127b05f8c5eaa2846199a38c1fadd369fab013
jberneche <javier@ubuntu.(none)>
2014-01-14 16:43:58 -0200

    contactables full text search on client-side...

--- client/js/contactables.js
+++ client/js/contactables.js
@@ -1,41 +1,63 @@
 ContactablesController = RouteController.extend({
-	template: 'contactables',
-	layoutTemplate: 'mainLayout',
+    template: 'contactables',
+    layoutTemplate: 'mainLayout',
 });
 
 Template.contactables.rendered = function () {
-	var viewModel = function () {
-		var self = this;
-		self.filter = ko.observable({});
-		self.entities = ko.meteor.find(Contactables, {});
-		self.filter = ko.observableArray([{
-			check: ko.observable(true),
-			label: 'isEmployee'
+    var viewModel = function () {
+        var self = this;
+        self.filter = ko.observable({});
+        self.entities = ko.meteor.find(Contactables, {});
+        self.searchString = ko.observable('');
+        self.filter = ko.observableArray([{
+            check: ko.observable(true),
+            label: 'isEmployee'
         }, {
-			check: ko.observable(true),
-			label: 'isContact'
+            check: ko.observable(true),
+            label: 'isContact'
         }, {
-			check: ko.observable(true),
-			label: 'isCustomer'
+            check: ko.observable(true),
+            label: 'isCustomer'
         }]);
-		var search = function () {
-			var q = {
-				$or: []
-			};
-			_.each(self.filter(), function (elem) {
-				if (elem.check()) {
-					var aux = {}
-					aux[elem.label] = elem.check();
-					q.$or.push(aux);
-				}
-			})
-			console.dir(q);
-			self.entities(ko.mapping.fromJS(Contactables.find(q).fetch())());
-		};
-		_.forEach(self.filter(), function (prop) {
-			prop.check.subscribe(search);
-		})
-	};
+        var search = function () {
+            var q = {
+                $or: []
+            };
+            _.each(self.filter(), function (elem) {
+                if (elem.check()) {
+                    var aux = {}
+                    aux[elem.label] = elem.check();
+                    q.$or.push(aux);
+                }
+            })
+            console.dir(q);
+            self.entities(ko.mapping.fromJS(Contactables.find(q).fetch())());
+        };
+        _.forEach(self.filter(), function (prop) {
+            prop.check.subscribe(search);
+        })
+        self.searchString.subscribe(function (value) {
+            self.entities(ko.mapping.fromJS(Contactables.find({
+                '$or': [{
+                    'firstName': {
+                        $regex: value
+                    }
+                            }, {
+                    'lastName': {
+                        $regex: value
+                    }
+                        }, {
+                    'organizationName': {
+                        $regex: value
+                    }
+                        }, {
+                    'tags': {
+                        $regex: value
+                    }
+                        }]
+            }).fetch())());
+        });
+    };
 
-	ko.applyBindings(new viewModel(), document.getElementsByName('contactablesVM')[0]);
+    ko.applyBindings(new viewModel(), document.getElementsByName('contactablesVM')[0]);
 };
