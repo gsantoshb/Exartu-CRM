9337e0e87e42e139d9666bc2830918755942a22e
jberneche <javier@ubuntu.(none)>
2014-01-14 14:45:00 -0200

    Fixing a bug in tags and contact methods, when the contactable hasn't that field set. Find a problem with the meteor's subscribe, so implement a temporarily solutions using observer patron

--- client/collections.js
+++ client/collections.js
@@ -1,5 +1,16 @@
 Contactables = new Meteor.Collection("contactables");
-Meteor.subscribe('contactables');
+ContactableHandler = Meteor.subscribe('contactables', function () {
+    _.forEach(ContactableHandler.observers, function (cb) {
+        cb();
+    });
+});
+ContactableHandler.observers = [];
+ContactableHandler.wait = function (cb) {
+    if (this.ready())
+        cb();
+    else
+        this.observers.push(cb);
+}
 
 Messages = new Meteor.Collection("messages");
 Meteor.subscribe('messages');
