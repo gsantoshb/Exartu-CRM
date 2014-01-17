7c296247d816450fbd77c27de943292344fe5696
psoto <psoto23.ps@gmail.com>
2014-01-17 16:31:51 -0600

    EmployeeType

---
+++ server/collections/contactableTypes/employee.js
@@ -0,0 +1,24 @@
+fieldType = {
+	string: 0,
+	int: 1,
+	date: 2,
+	select: 3,
+	checkbox: 4,
+};
+
+EmployeeType = {
+	_id: 0,
+	name: 'Employee',
+	services: ['messages'],
+	fields: [{
+		name: 'test',
+		regex: /.+/,
+		type: fieldType.string,
+		defaultValue: '',
+	}, {
+		name: 'test2',
+		regex: /.+/,
+		type: fieldType.string,
+		defaultValue: '',
+	}]
+}
