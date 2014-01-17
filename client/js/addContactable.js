d940dc916ae80e62e4da311df0e61d70965c87cf
psoto <psoto23.ps@gmail.com>
2014-01-17 17:41:20 -0600

    Remove hack

--- client/js/addContactable.js
+++ client/js/addContactable.js
@@ -1,57 +1,54 @@
 Template.addContactable.viewmodel = function (typeId) {
-    var self = this;
+	var self = this;
 
-    self.contactable = {
+	self.contactable = {
 		type: [typeId]
-    };
+	};
 
-    _.extend(self.contactable, {
-        person: Global.person
-    });
+	_.extend(self.contactable, {
+		person: Global.person
+	});
 
-	self.contactable.person.firstName = "pablo";
-	self.contactable.person.lastName = "soto";
+	self.fields = ko.observableArray();
+	Meteor.call('getFields', typeId, function (err, result) {
+		if (!err) {
+			_.forEach(result, function (item) {
+				return _.extend(item, {
+					value: ko.observable()
+				})
+			});
+			self.fields(result);
+			//debugger;
+		}
+	});
 
-    self.fields = ko.observableArray();
-    Meteor.call('getFields', typeId, function (err, result) {
-        if (!err) {
-            _.forEach(result, function (item) {
-                return _.extend(item, {
-                    value: ko.observable()
-                })
-            });
-            self.fields(result);
-            //debugger;
-        }
-    });
-
-    self.addContactable = function () {
-        var names = _.map(self.fields(), function (item) {
-            return item.name;
-        });
-        var values = _.map(self.fields(), function (item) {
-            return item.value();
-        })
-        var employee = _.object(names, values);
-        _.extend(self.contactable, {
+	self.addContactable = function () {
+		var names = _.map(self.fields(), function (item) {
+			return item.name;
+		});
+		var values = _.map(self.fields(), function (item) {
+			return item.value();
+		})
+		var employee = _.object(names, values);
+		_.extend(self.contactable, {
 			Employee: employee
-        });
+		});
 
-        Meteor.call('addContactable', self.contactable);
-        $('#addContactableModal').modal('hide');
+		Meteor.call('addContactable', self.contactable);
+		$('#addContactableModal').modal('hide');
 		//debugger;
-    }
-    return this;
+	}
+	return this;
 }
 
 Meteor.methods({
-    addContactable: function (contactable) {
-        Contactables.insert(contactable);
-    },
-    getFields: function (id) {
+	addContactable: function (contactable) {
+		Contactables.insert(contactable);
+	},
+	getFields: function (id) {
 
-    },
-    getContactablesType: function () {
+	},
+	getContactablesType: function () {
 
-    }
+	}
 });
