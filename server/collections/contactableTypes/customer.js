d1218ff77dca30cc0ad9af93add55753b6e97135
jberneche <javier@ubuntu.(none)>
2014-01-20 18:09:21 -0200

    - fix last commit -fix add customer (client) -fix add organization (server)

--- server/collections/contactableTypes/customer.js
+++ server/collections/contactableTypes/customer.js
@@ -1,24 +1,24 @@
 fieldType = {
-	string: 0,
-	int: 1,
-	date: 2,
-	select: 3,
-	checkbox: 4
+    string: 0,
+    int: 1,
+    date: 2,
+    select: 3,
+    checkbox: 4
 };
 
 CustomerType = {
-	_id: 1,
-	name: 'Customer',
-	services: ['messages', 'tasks'],
-	fields: [{
-		name: 'test',
-		regex: /./,
-		type: fieldType.string,
-		defaultValue: ''
-	}, {
-		name: 'test2',
-		regex: /./,
-		type: fieldType.string,
-		defaultValue: ''
-	}]
+    _id: 1,
+    name: 'Customer',
+    services: ['messages', 'tasks'],
+    fields: [{
+        name: 'department',
+        regex: /./,
+        type: fieldType.string,
+        defaultValue: ''
+ }, {
+        name: 'test2',
+        regex: /./,
+        type: fieldType.string,
+        defaultValue: ''
+ }]
 }
