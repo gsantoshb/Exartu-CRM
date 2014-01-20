cd210ede5f026d91cd90da8a030bc9eb6cf7e5b2
jberneche <javier@ubuntu.(none)>
2014-01-20 18:31:36 -0200

    fix search and filter

--- client/js/contactables.js
+++ client/js/contactables.js
@@ -1,88 +1,94 @@
 ContactablesController = RouteController.extend({
-	template: 'contactables',
-	layoutTemplate: 'mainLayout'
+    template: 'contactables',
+    layoutTemplate: 'mainLayout'
 });
 
 Template.contactables.rendered = function () {
-	var viewModel = function () {
-		var self = this;
-		self.filter = ko.observable({});
-		self.entities = ko.meteor.find(Contactables, {});
-		self.searchString = ko.observable('');
+    var viewModel = function () {
+        var self = this;
+        self.filter = ko.observable({});
+        self.entities = ko.meteor.find(Contactables, {});
+        self.searchString = ko.observable('');
 
+        var propsWhereSearch = ['person.firstName', 'person.lastName', 'organization.organizationName'];
 
-		var search = function () {
-			var q = {
-				$or: []
-			};
-			_.each(self.filter(), function (elem) {
-				if (elem.check()) {
-					var aux = {}
-					aux[elem.label] = {
-						$exists: true
-					};
-					q.$or.push(aux);
-				}
-			})
-			console.dir(q);
-			if (q.$or.length == 0)
-				q = {};
+        var search = function () {
+            //            debugger;
+            var q = {};
+            var search;
+            var filter;
+            if (self.searchString()) {
+                q.$and = [];
+                q.$and.push({
+                    $or: []
+                });
+                search = q.$and[0].$or;
 
-			self.entities(ko.mapping.fromJS(Contactables.find(q).fetch())());
-		};
+                q.$and.push({
+                    $or: []
+                });
+                filter = q.$and[1].$or;
 
-		self.filter = ko.observableArray();
-		self.contactableTypes = ko.observableArray();
-		Meteor.call('getContactableTypes', function (err, result) {
-			if (!err) {
-				self.contactableTypes(result);
-				self.filter(
-					_.map(result, function (type) {
-						filter = {
-							check: ko.observable(true),
-							label: type.name,
-							typeId: type._id
-						};
-						filter.check.subscribe(search);
+                _.each(propsWhereSearch, function (prop) {
+                    var aux = {};
+                    aux[prop + ''] = {
+                        $regex: self.searchString()
+                    };
+                    search.push(aux);
+                });
+            } else {
+                q = {
+                    $or: []
+                };
+                filter = q.$or;
+            }
 
-						return filter;
-					})
-				);
+            _.each(self.filter(), function (elem) {
+                if (elem.check()) {
+                    var aux = {}
+                    aux[elem.label] = {
+                        $exists: true
+                    };
+                    filter.push(aux);
+                }
+            })
+            //            console.dir(q);
 
-				//debugger;
-			}
-		});
+            self.entities(ko.mapping.fromJS(Contactables.find(q).fetch())());
+        };
 
-		self.showAddContactableModal = function (typeId) {
-			Session.set('newContactableTypeId', typeId);
-			$('#addContactableModal').modal('show');
-		};
+        self.filter = ko.observableArray();
+        self.contactableTypes = ko.observableArray();
+        Meteor.call('getContactableTypes', function (err, result) {
+            if (!err) {
+                self.contactableTypes(result);
+                self.filter(
+                    _.map(result, function (type) {
+                        filter = {
+                            check: ko.observable(true),
+                            label: type.name,
+                            typeId: type._id
+                        };
+                        filter.check.subscribe(search);
 
-		_.forEach(self.filter(), function (prop) {
-			prop.check.subscribe(search);
-		})
+                        return filter;
+                    })
+                );
 
-		self.searchString.subscribe(function (value) {
-			self.entities(ko.mapping.fromJS(Contactables.find({
-				'$or': [{
-					'firstName': {
-						$regex: value
-					}
-                            }, {
-					'lastName': {
-						$regex: value
-					}
-                        }, {
-					'organizationName': {
-						$regex: value
-					}
-                        }, {
-					'tags': {
-						$regex: value
-					}
-                        }]
-			}).fetch())());
-		});
-	};
-	helper.applyBindings(viewModel, 'contactablesVM');
+                //debugger;
+            }
+        });
+
+        self.showAddContactableModal = function (typeId) {
+            Session.set('newContactableTypeId', typeId);
+            $('#addContactableModal').modal('show');
+        };
+
+        _.forEach(self.filter(), function (prop) {
+            prop.check.subscribe(search);
+        })
+
+        self.searchString.subscribe(search);
+    };
+    helper.applyBindings(viewModel, 'contactablesVM');
 };
