42490cf6b09c1ef49cddb2a3c905abf8e54c19a8
gregg dourgarian <greggd@tempworks.com>
2014-01-21 07:36:31 -0600

    add debugger that can be turned on with setting Global.debugg=true in console

---
+++ client/js/debuggers.js
@@ -0,0 +1,42 @@
+
+function logRenders () {
+    _.each(Template, function (template, name) {
+        var oldRender = template.rendered;
+        var counter = 0;
+
+        template.rendered = function () {
+            console.log(name, "render count: ", ++counter);
+            oldRender && oldRender.apply(this, arguments);
+        };
+    });
+}
+
+var wrappedFind = Meteor.Collection.prototype.find;
+
+Meteor.Collection.prototype.find = function () {
+    var cursor = wrappedFind.apply(this, arguments);
+    if (Global.debugger)
+    {
+        var collectionName = this._name;
+
+        cursor.observeChanges({
+            added: function (id, fields) {
+                console.log(collectionName, 'added', id, fields);
+            },
+
+            changed: function (id, fields) {
+                console.log(collectionName, 'changed', id, fields);
+            },
+
+            movedBefore: function (id, before) {
+                console.log(collectionName, 'movedBefore', id, before);
+            },
+
+            removed: function (id) {
+                console.log(collectionName, 'removed', id);
+            }
+            });
+    }
+
+    return cursor;
+};
