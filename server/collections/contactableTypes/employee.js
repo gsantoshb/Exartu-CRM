da7b251748bfd29dd5019292b27a31c1be414b41
jberneche <javier@ubuntu.(none)>
2014-01-21 16:42:32 -0200

    fix employee fields (set showInAdd=true)

--- server/collections/contactableTypes/employee.js
+++ server/collections/contactableTypes/employee.js
@@ -1,24 +1,26 @@
 fieldType = {
-	string: 0,
-	int: 1,
-	date: 2,
-	select: 3,
-	checkbox: 4
+    string: 0,
+    int: 1,
+    date: 2,
+    select: 3,
+    checkbox: 4
 };
 
 EmployeeType = {
-	_id: 0,
-	name: 'Employee',
-	services: ['messages', 'tasks'],
-	fields: [{
-		name: 'test',
-		regex: /./,
-		type: fieldType.string,
-		defaultValue: ''
-	}, {
-		name: 'test2',
-		regex: /./,
-		type: fieldType.string,
-		defaultValue: ''
-	}]
+    _id: 0,
+    name: 'Employee',
+    services: ['messages', 'tasks'],
+    fields: [{
+        name: 'test',
+        regex: /.*/,
+        type: fieldType.string,
+        defaultValue: '',
+        showInAdd: true
+ }, {
+        name: 'test2',
+        regex: /./,
+        type: fieldType.string,
+        defaultValue: '',
+        showInAdd: true
+ }]
 }
