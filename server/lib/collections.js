9191f3d8ce0c9086e1dddd79e127b675697957a7
psoto <psoto23.ps@gmail.com>
2014-01-21 17:06:50 -0600

    Move collection definition to server/lib/collections.js to avoid dependecies issues

---
+++ server/lib/collections.js
@@ -0,0 +1,9 @@
+Hierarchies = new Meteor.Collection("hierarchies");
+
+Jobs = new Meteor.Collection("jobs");
+
+Activities = new Meteor.Collection("activities");
+
+Contactables = new Meteor.Collection("contactables");
+
+Messages = new Meteor.Collection("messages");
