85b953cad78209ac70e00ae8b56e91459a92d800
psoto <psoto23.ps@gmail.com>
2014-01-21 17:05:02 -0600

    Move fieldType into Enums

--- server/collections/contactableTypes/customerContact.js
+++ server/collections/contactableTypes/customerContact.js
@@ -1,24 +1,17 @@
-fieldType = {
-    string: 0,
-    int: 1,
-    date: 2,
-    select: 3,
-    checkbox: 4
-};
-
 CustomerContactType = {
-    _id: 3,
-    name: 'CustomerContact',
-    services: ['messages', 'tasks'],
-    fields: [{
-        name: 'test',
-        regex: /./,
-        type: fieldType.string,
-        defaultValue: ''
-    }, {
-        name: 'test2',
-        regex: /./,
-        type: fieldType.string,
-        defaultValue: ''
+	_id: 3,
+	contactableType: true,
+	name: 'CustomerContact',
+	services: ['messages', 'tasks'],
+	fields: [{
+		name: 'test',
+		regex: /./,
+		type: Enums.fieldType.string,
+		defaultValue: ''
+	}, {
+		name: 'test2',
+		regex: /./,
+		type: Enums.fieldType.string,
+		defaultValue: ''
     }]
 }
--- server/collections/contactableTypes/employee.js
+++ server/collections/contactableTypes/employee.js
@@ -1,24 +1,17 @@
-fieldType = {
-	string: 0,
-	int: 1,
-	date: 2,
-	select: 3,
-	checkbox: 4
-};
-
 EmployeeType = {
 	_id: 0,
+	contactableType: true,
 	name: 'Employee',
 	services: ['messages', 'tasks'],
 	fields: [{
 		name: 'test',
 		regex: /./,
-		type: fieldType.string,
+		type: Enums.fieldType.string,
 		defaultValue: ''
 	}, {
 		name: 'test2',
 		regex: /./,
-		type: fieldType.string,
+		type: Enums.fieldType.string,
 		defaultValue: ''
 	}]
 }
