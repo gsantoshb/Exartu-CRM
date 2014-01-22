94513003d343651b26b76296c65188547b3a3700
psoto <psoto23.ps@gmail.com>
2014-01-22 16:25:19 -0600

    Add job Basic objType for job Data in jobs list pending

--- server/collections/contactableTypes/customer.js
+++ server/collections/contactableTypes/customer.js
@@ -1,48 +1,48 @@
 var isContact = {};
 isContact['CustomerContact'] = {
-    $exists: true
+	$exists: true
 }
 
 CustomerType = {
-    _id: 1,
-    contactableType: true,
-    name: 'Customer',
-    services: ['messages', 'tasks'],
-    fields: [{
-        name: 'department',
-        regex: '.',
-        type: Enums.fieldType.string,
-        defaultValue: '',
-        showInAdd: true,
+	_id: 1,
+	contactableType: true,
+	name: 'Customer',
+	services: ['messages', 'tasks'],
+	fields: [{
+		name: 'department',
+		regex: '.',
+		type: Enums.fieldType.string,
+		defaultValue: '',
+		showInAdd: true,
     }, {
-        name: 'test2',
-        regex: '.',
-        type: Enums.fieldType.string,
-        defaultValue: '',
-        showInAdd: false,
+		name: 'test2',
+		regex: '.*',
+		type: Enums.fieldType.string,
+		defaultValue: '',
+		showInAdd: false,
     }],
-    relations: [{
-        name: 'contacts',
-        target: {
-            collection: 'Contactables',
-            query: isContact
-        },
-        cardinality: {
-            min: 0,
-            max: Infinity
-        },
-        defaultValue: [],
-        validate: function (value) {
-            if (Object.prototype.toString.call(value) === '[object Array]') {
-                var r = true;
-                _.forEach(value, function (v) {
-                    r = r && (v.type.indexOf[CustomerContactType._id] >= 0);
-                })
-                return r;
-            } else {
-                return (v.type.indexOf[CustomerContactType._id] >= 0);
-            }
-        },
-        showInAdd: false,
+	relations: [{
+		name: 'contacts',
+		target: {
+			collection: 'Contactables',
+			query: isContact
+		},
+		cardinality: {
+			min: 0,
+			max: Infinity
+		},
+		defaultValue: [],
+		validate: function (value) {
+			if (Object.prototype.toString.call(value) === '[object Array]') {
+				var r = true;
+				_.forEach(value, function (v) {
+					r = r && (v.type.indexOf[CustomerContactType._id] >= 0);
+				})
+				return r;
+			} else {
+				return (v.type.indexOf[CustomerContactType._id] >= 0);
+			}
+		},
+		showInAdd: false,
     }]
 }
