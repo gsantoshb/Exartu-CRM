a117c9d61677efc72ffb9fe8a3c821c190ae30d9
jberneche <javier@ubuntu.(none)>
2014-01-24 13:06:53 -0200

    modifing relations to work on saved relations saved in the db
    --this commit is probably broken

--- server/main.js
+++ server/main.js
@@ -1,140 +1,189 @@
 var dbSeed = {
-	/*
-	 * Add to system hierarchy the basic object types
-	 * 	Contactable:
-	 *    - Customer
-	 *    - Employee
-	 *    - Contact
-	 *  Job:
-	 *    - Permanent
-	 *    - Temporal
-	 */
-	seedSystemObjTypes: function () {
-		var systemObjTypes = [
-			{
-				type: Enums.objectTypeTypes.contactable,
-				name: 'Customer',
-				services: ['messages', 'tasks'],
-				fields: [{
-					name: 'department',
-					regex: '.',
-					type: Enums.fieldType.string,
-					defaultValue: '',
-					showInAdd: true,
-				}, {
-					name: 'test2',
-					regex: '.*',
-					type: Enums.fieldType.string,
-					defaultValue: '',
-					showInAdd: false,
-				}],
-			},
-			{
-				type: Enums.objectTypeTypes.contactable,
-				name: 'CustomerContact',
-				services: ['messages', 'tasks'],
-				fields: [{
-					name: 'test',
-					regex: '^(([1-9][0-9]*|[0-9])(.[0-9]*|))$',
-					type: Enums.fieldType.string,
-					defaultValue: '',
-					showInAdd: true
-				}, {
-					name: 'test2',
-					regex: '^[a-z0-9].$',
-					type: Enums.fieldType.string,
-					defaultValue: '',
-					showInAdd: true
-				}],
-			},
-			{
-				type: Enums.objectTypeTypes.contactable,
-				name: 'Employee',
-				services: ['messages', 'tasks'],
-				fields: [{
-					name: 'test',
-					regex: '.*',
-					type: Enums.fieldType.string,
-					defaultValue: '',
-					showInAdd: true
-				}, {
-					name: 'test2',
-					regex: '.*',
-					type: Enums.fieldType.string,
-					defaultValue: '',
-					showInAdd: true
- 				}]
-			},
-			{
-				type: Enums.objectTypeTypes.job,
-				name: 'Permanent',
-				services: ['messages', 'tasks'],
-				fields: [{
-					name: 'test',
-					regex: '.*',
-					type: Enums.fieldType.string,
-					defaultValue: '',
-					showInAdd: true
-				}, {
-					name: 'test2',
-					regex: '.*',
-					type: Enums.fieldType.string,
-					defaultValue: '',
-					showInAdd: true
- 				}]
-			},
-			{
-				type: Enums.objectTypeTypes.job,
-				name: 'Temporal',
-				services: ['messages', 'tasks'],
-				fields: [{
-					name: 'Type',
-					regex: '.*',
-					type: Enums.fieldType.string,
-					defaultValue: '',
-					showInAdd: true
-				}, {
-					name: 'test2',
-					regex: '.*',
-					type: Enums.fieldType.string,
-					defaultValue: '',
-					showInAdd: true
- 				}]
-			}
-		];
+    /*
+     * Add to system hierarchy the basic object types
+     * 	Contactable:
+     *    - Customer
+     *    - Employee
+     *    - Contact
+     *  Job:
+     *    - Permanent
+     *    - Temporal
+     */
+    seedSystemObjTypes: function () {
+        var systemObjTypes = [
+            {
+                type: Enums.objectTypeTypes.contactable,
+                name: 'Customer',
+                services: ['messages', 'tasks'],
+                fields: [{
+                    name: 'department',
+                    regex: '.',
+                    type: Enums.fieldType.string,
+                    defaultValue: '',
+                    showInAdd: true,
+                    }, {
+                    name: 'test2',
+                    regex: '.*',
+                    type: Enums.fieldType.string,
+                    defaultValue: '',
+                    showInAdd: false,
+                }],
+            },
+            {
+                type: Enums.objectTypeTypes.contactable,
+                name: 'CustomerContact',
+                services: ['messages', 'tasks'],
+                fields: [{
+                    name: 'test',
+                    regex: '^(([1-9][0-9]*|[0-9])(.[0-9]*|))$',
+                    type: Enums.fieldType.string,
+                    defaultValue: '',
+                    showInAdd: true
+                    }, {
+                    name: 'test2',
+                    regex: '^[a-z0-9].$',
+                    type: Enums.fieldType.string,
+                    defaultValue: '',
+                    showInAdd: true
+                }],
+            },
+            {
+                type: Enums.objectTypeTypes.contactable,
+                name: 'Employee',
+                services: ['messages', 'tasks'],
+                fields: [{
+                    name: 'test',
+                    regex: '.*',
+                    type: Enums.fieldType.string,
+                    defaultValue: '',
+                    showInAdd: true
+                    }, {
+                    name: 'test2',
+                    regex: '.*',
+                    type: Enums.fieldType.string,
+                    defaultValue: '',
+                    showInAdd: true
+                }]
+            },
+            {
+                type: Enums.objectTypeTypes.job,
+                name: 'Permanent',
+                services: ['messages', 'tasks'],
+                fields: [{
+                    name: 'test',
+                    regex: '.*',
+                    type: Enums.fieldType.string,
+                    defaultValue: '',
+                    showInAdd: true
+                    }, {
+                    name: 'test2',
+                    regex: '.*',
+                    type: Enums.fieldType.string,
+                    defaultValue: '',
+                    showInAdd: true
+                }]
+            },
+            {
+                type: Enums.objectTypeTypes.job,
+                name: 'Temporal',
+                services: ['messages', 'tasks'],
+                fields: [{
+                    name: 'Type',
+                    regex: '.*',
+                    type: Enums.fieldType.string,
+                    defaultValue: '',
+                    showInAdd: true
+                    }, {
+                    name: 'test2',
+                    regex: '.*',
+                    type: Enums.fieldType.string,
+                    defaultValue: '',
+                    showInAdd: true
+                }]
+            }
+        ];
 
-		_.forEach(systemObjTypes, function (objType) {
-			var type = ObjectTypes.findOne({
-				name: objType.name
-			});
-			if (type == null) {
-				ObjectTypes.insert({
-					hierId: ExartuConfig.SystemHierarchyId,
-					type: objType.type,
-					name: objType.name,
-					services: objType.services,
-					fields: objType.fields,
-				})
-			} else {
-				ObjectTypes.update({
-					_id: type._id
-				}, {
-					$set: {
-						services: objType.services,
-						fields: objType.fields,
-					}
-				})
-			}
-		});
-	},
+        _.forEach(systemObjTypes, function (objType) {
+            var type = ObjectTypes.findOne({
+                name: objType.name
+            });
+            if (type == null) {
+                ObjectTypes.insert({
+                    hierId: ExartuConfig.SystemHierarchyId,
+                    type: objType.type,
+                    name: objType.name,
+                    services: objType.services,
+                    fields: objType.fields,
+                })
+            } else {
+                ObjectTypes.update({
+                    _id: type._id
+                }, {
+                    $set: {
+                        services: objType.services,
+                        fields: objType.fields,
+                    }
+                })
+            }
+        });
+    },
+    seedSystemRelations: function () {
+        var systemRelations = [
+            {
+                name: 'CustomerContacts',
+                obj1: 'CustomerContacts',
+                obj2: 'Customer',
+                visibilityOn1: {
+                    name: 'customer',
+                    collection: 'Contactables',
+                    defaultValue: null,
+                    cardinality: {
+                        min: 0,
+                        max: 1
+                    },
+                },
+                visibilityOn2: {
+                    name: 'contacts',
+                    collection: 'Contactables',
+                    defaultValue: null,
+                    cardinality: {
+                        min: 0,
+                        max: Infinity
+                    },
+                },
+                cascadeDelete: false,
+            },
+        ];
+
+        _.forEach(systemRelations, function (rel) {
+            var oldRel = Relations.findOne({
+                name: rel.name
+            });
+            if (oldRel == null) {
+                rel.hierId = ExartuConfig.SystemHierarchyId;
+                console.dir(rel);
+                Relations.insert(rel);
+            } else {
+                Relations.update({
+                    _id: oldRel._id
+                }, {
+                    $set: {
+                        visibilityOn1: rel.visibilityOn1,
+                        visibilityOn2: rel.visibilityOn2,
+                        cascadeDelete: rel.cascadeDelete
+                    }
+                })
+            }
+        });
+    }
 }
 
 Meteor.startup(function () {
-	/*
-	 * Seed database
-	 * Execute all function defined in seedSystemObjTypes
-	 */
-	_.forEach(dbSeed, function (seedFn) {
-		seedFn.call();
-	})
+    /*
+     * Seed database
+     * Execute all function defined in seedSystemObjTypes
+     */
+    _.forEach(dbSeed, function (seedFn) {
+        seedFn.call();
+    })
 });
