3b9942883277f41ca0269cb0c5ff9838c861d5f0
psoto <psoto23.ps@gmail.com>
2014-01-24 15:41:07 -0600

    Object types relations refactor Now relation are only defined in Relations collections, not in each object tpye

--- server/collections/contactable.js
+++ server/collections/contactable.js
@@ -1,113 +1,115 @@
 Meteor.publish('contactables', function () {
-    var user = Meteor.users.findOne({
-        _id: this.userId
-    });
+	var user = Meteor.users.findOne({
+		_id: this.userId
+	});
 
-    if (!user)
-        return false;
+	if (!user)
+		return false;
 
-    return Contactables.find({
-        hierId: user.hierId
-    });
+	return Contactables.find({
+		hierId: user.hierId
+	});
 })
 
 Meteor.startup(function () {
-    Meteor.methods({
-        addContactable: function (contactable) {
-            var user = Meteor.user();
-            if (user == null)
-                throw new Meteor.Error(401, "Please login");
+	Meteor.methods({
+		addContactable: function (contactable) {
+			var user = Meteor.user();
+			if (user == null)
+				throw new Meteor.Error(401, "Please login");
 
-            addSystemMetadata(contactable, user);
+			addSystemMetadata(contactable, user);
 
-            if (extendAndValidate(contactable)) {
-                Contactables.insert(contactable);
-            } else {
-                console.error('Contactable not valid')
-                console.dir(contactable);
-            }
-        },
-        updateContactable: function (contactable) {
-            var user = Meteor.user();
-            if (user == null)
-                throw new Meteor.Error(401, "Please login");
-            //todo: check hier and roles
-            if (extendAndValidate(contactable)) {
-                Contactables.update({
-                    _id: contactable.id
-                }, contactable);
-            } else {
-                console.error('Contactable not valid')
-                console.dir(contactable);
-            }
+			if (extendAndValidate(contactable)) {
+				Contactables.insert(contactable);
+			} else {
+				console.error('Contactable not valid')
+				console.dir(contactable);
+			}
+		},
+		updateContactable: function (contactable) {
+			var user = Meteor.user();
+			if (user == null)
+				throw new Meteor.Error(401, "Please login");
+			//todo: check hier and roles
+			if (extendAndValidate(contactable)) {
+				Contactables.update({
+					_id: contactable._id
+				}, contactable);
+			} else {
+				console.error('Contactable not valid')
+				console.dir(contactable);
+			}
 
-        }
-    });
+		}
+	});
 });
 
-//Contactables.allow({
-//    update: function () {
-//        return false;
-//    }
-//});
-
 Contactables.before.insert(function (userId, doc) {
-    doc.createdAt = Date.now();
+	doc.createdAt = Date.now();
 });
 
 var extendAndValidate = function (contactable) {
-    //contactable's things
-    if (!contactable.contactMethods)
-        contactable.contactMethods = [];
+	//contactable's things
+	if (!contactable.contactMethods)
+		contactable.contactMethods = [];
 
-    if (!contactable.type || !contactable.type.length) {
-        console.log('the contactable must have a type');
-        return false;
-    }
-    var v = true;
-    //add the services defined in the types
-    _.forEach(contactable.type, function (type) {
-        var ObjType = _.findWhere(ObjectTypes, {
-            _id: type
-        });
-        _.forEach(ObjType.services, function (service) {
-            contactable[service] = [];
-        });
-        v = v && validateObjType(contactable, type);
-    });
+	if (!contactable.type || !contactable.type.length) {
+		console.log('the contactable must have a type');
+		return false;
+	}
+	var v = true;
+	//add the services defined in the types
+	_.every(contactable.type, function (type) {
+		var ObjType = ObjectTypes.findOne({
+			name: type
+		});
+		if (ObjType != null) {
+			_.forEach(ObjType.services, function (service) {
+				contactable[service] = [];
+			});
 
-    return v && validateContactable(contactable);
+			v = v && validateObjType(contactable, ObjType);
+		} else {
+			console.error('invalid object type: ' + type);
+			v = false;
+		}
+
+		return v;
+	});
+
+	return v && validateContactable(contactable);
 };
 
 var validateContactable = function (obj) {
-    if (!obj.person & !obj.organization) {
-        console.error('the contactable must be a person or an organization');
-        return false
-    }
-    if (obj.person && (!validatePerson(obj.person))) {
-        console.error('invalid person');
-        return false;
-    }
-    if (obj.organization && (!validateOrganization(obj.organization))) {
-        console.error('invalid Organization');
-        return false;
-    }
+	if (!obj.person & !obj.organization) {
+		console.error('the contactable must be a person or an organization');
+		return false
+	}
+	if (obj.person && (!validatePerson(obj.person))) {
+		console.error('invalid person');
+		return false;
+	}
+	if (obj.organization && (!validateOrganization(obj.organization))) {
+		console.error('invalid Organization');
+		return false;
+	}
 
-    return true;
+	return true;
 };
 
 var validatePerson = function (person) {
-    if (!person.firstName)
-        return false;
-    if (!person.lastName)
-        return false;
+	if (!person.firstName)
+		return false;
+	if (!person.lastName)
+		return false;
 
-    return true;
+	return true;
 };
 
 var validateOrganization = function (org) {
-    if (!org.organizationName)
-        return false;
+	if (!org.organizationName)
+		return false;
 
-    return true;
+	return true;
 }
--- server/collections/objectType.js
+++ server/collections/objectType.js
@@ -10,70 +10,73 @@
  *  - objTypeId: Id of type used to validated the object (obj)
  */
 
-validateObjType = function (obj, objTypeId) {
-    var objType = _.findWhere(ObjectTypes, {
-        _id: objTypeId
-    });
+validateObjType = function (obj, objType) {
+	if (!objType) {
+		console.error('Object type does not exists');
+		return false;
+	}
 
-    if (!objType) {
-        console.error('Object type does not exists');
-        return false;
-    }
+	if (obj.type.indexOf(objType.name) < 0) {
+		console.error('Objtype incorrect');
+		return false;
+	}
 
-    if (obj.type.indexOf(objTypeId) < 0) {
-        console.error('Objtype incorrect');
-        return false;
-    }
+	// Validating services
+	var v = true;
+	_.every(SystemServices, function (service) {
+		var needed = objType.services.indexOf(service) >= 0;
+		var used = obj[service] ? true : false;
 
-    // Validating services
-    var v = true;
-    _.every(Services, function (service) {
-        var needed = objType.services.indexOf(service) >= 0;
-        var used = obj[service] ? true : false;
+		v = ((needed && used) || (!needed && !used));
+		return v;
+	});
+	if (!v) {
+		console.error('invalid service');
+		return false;
+	}
 
-        v = ((needed && used) || (!needed && !used));
-        return v;
-    });
-    if (!v) {
-        console.error('invalid service');
-        return false;
-    }
+	// Validating fields
+	var typeInfo = obj[objType.name];
+	v = true;
+	_.every(objType.fields, function (field) {
+		if (typeInfo[field.name] != undefined) {
+			v = v && (typeInfo[field.name].match(field.regex) != null);
+			if (!v) console.error(field.name + ' is invalid: ' + v);
+			return v;
+		} else {
+			v = false;
+			console.error(field.name + ' does not exists');
+			return false;
+		}
+	});
+	if (!v) {
+		console.error('invalid fields');
+		return false;
+	}
 
-    // Validating fields
-    var typeInfo = obj[objType.name];
-    v = true;
-    _.every(objType.fields, function (field) {
-        if (typeInfo[field.name] != undefined) {
-            v = v && (typeInfo[field.name].match(field.regex) != null);
-            if (!v) console.error(field.name + ' is invalid: ' + v);
-            return v;
-        } else {
-            v = false;
-            console.error(field.name + ' does not exists');
-            return false;
-        }
-    });
-    if (!v) {
-        console.error('invalid fields');
-        return false;
-    }
-    _.every(objType.relations, function (rel) {
-        if (typeInfo[rel.name] != undefined) {
-            v = v && beforeUpdateRelation(typeInfo[rel.name], rel);
-            if (!v) console.error(rel.name + ' is invalid: ' + v);
-            return v;
-        } else {
-            v = false;
-            console.error(rel.name + ' does not exists');
-            return false;
-        }
-    });
-    if (!v) {
-        console.error('invalid relations');
-        return false;
-    }
+	// Validating relations
+	var relations = Relations.find({
+		$or: [{
+			obj1: objType.name
+		}, {
+			obj2: objType.name
+		}]
+	}).fetch();
+	_.forEach(relations, function (rel) {
+		var objRel = typeInfo.name == rel.obj1 ? rel.visibilityOn1 : rel.visibilityOn2;
+		if (typeInfo[rel.name] != undefined) {
+			v = v && beforeUpdateRelation(typeInfo[rel.name], objRel);
+			if (!v) console.error(rel.name + ' is invalid: ' + v);
+		} else {
+			typeInfo[objRel.name] = objRel.defaultValue;
+		}
+	});
+	if (!v) {
+		console.error('invalid relations');
+		return false;
+	}
 
-    return true;
+	return true;
 }
 
 /*
@@ -82,85 +85,85 @@ validateObjType = function (obj, objTypeId) {
 SystemServices = ['messages ', 'documents ', 'pastJobs ', 'tags ', 'education ', 'task '];
 
 Meteor.startup(function () {
-    Meteor.methods({
-        createObjectType: function (type, name, services, fields) {
-            // validate type
-            if (!_.contains(Enums.objectTypeTypes, type)) {
-                console.error('create objectType: objType type does not exists')
-                return null;
-            }
+	Meteor.methods({
+		createObjectType: function (type, name, services, fields) {
+			// validate type
+			if (!_.contains(Enums.objectTypeTypes, type)) {
+				console.error('create objectType: objType type does not exists')
+				return null;
+			}
 
-            // validate name
-            if (!(typeof name == 'string' || name instanceof String)) {
-                console.error('create objectType: objType name is not a string')
-                return null;
-            }
+			// validate name
+			if (!(typeof name == 'string' || name instanceof String)) {
+				console.error('create objectType: objType name is not a string')
+				return null;
+			}
 
-            if (ObjectTypes.findOne({
-                name: name
-            }) != null) {
-                console.error('create objectType: objectType name is already used');
-                return null;
-            }
+			if (ObjectTypes.findOne({
+				name: name
+			}) != null) {
+				console.error('create objectType: objectType name is already used');
+				return null;
+			}
 
-            // validate services
-            var v = true;
-            if (Object.prototype.toString.call(services) === '[object Array]')
-                _.every(services, function (service) {
-                    if (!_.contains(SystemServices, service)) {
-                        v = false;
-                        console.error('create objectType: service does not exists');
-                        return false;
-                    }
-                    return true;
-                });
-            else {
-                console.error('create objectType: objType services is not an array')
-                return null;
-            }
+			// validate services
+			var v = true;
+			if (Object.prototype.toString.call(services) === '[object Array]')
+				_.every(services, function (service) {
+					if (!_.contains(SystemServices, service)) {
+						v = false;
+						console.error('create objectType: service does not exists');
+						return false;
+					}
+					return true;
+				});
+			else {
+				console.error('create objectType: objType services is not an array')
+				return null;
+			}
 
-            // validate fields
-            if (Object.prototype.toString.call(fields) === '[object Array]')
-                _.every(fields, function (field) {
-                    if (!(typeof field.name == 'string' || field.name instanceof String)) {
-                        console.log('create objectType: field name invalid --> ' + field.name);
-                        v = false;
-                        return false;
-                    }
-                    if (!(typeof field.showInAdd == 'boolean')) {
-                        console.log('create objectType: field showInAdd invalid --> ' + field.name);
-                        v = false;
-                        return false;
-                    }
-                    // TODO: validate regex, type and default value
-                });
-            else {
-                console.error('create objectType: objType fields is not an array')
-                return null;
-            }
+			// validate fields
+			if (Object.prototype.toString.call(fields) === '[object Array]')
+				_.every(fields, function (field) {
+					if (!(typeof field.name == 'string' || field.name instanceof String)) {
+						console.log('create objectType: field name invalid --> ' + field.name);
+						v = false;
+						return false;
+					}
+					if (!(typeof field.showInAdd == 'boolean')) {
+						console.log('create objectType: field showInAdd invalid --> ' + field.name);
+						v = false;
+						return false;
+					}
+					// TODO: validate regex, type and default value
+				});
+			else {
+				console.error('create objectType: objType fields is not an array')
+				return null;
+			}
 
-            return ObjectTypes.insert({
-                hierId: Meteor.user().hierId,
-                type: type,
-                name: name,
-                services: services,
-                fields: fields,
-            })
-        },
-        getObjType: function (id) {
-            return ObjectTypes.findOne({
-                _id: id
-            });
-        },
-        getContactableTypes: function () {
-            return ObjectTypes.find({
-                type: Enums.objectTypeTypes.contactable
-            }).fetch();
-        },
-        getJobTypes: function () {
-            return ObjectTypes.find({
-                type: Enums.objectTypeTypes.job
-            }).fetch();
-        }
-    });
+			return ObjectTypes.insert({
+				hierId: Meteor.user().hierId,
+				type: type,
+				name: name,
+				services: services,
+				fields: fields,
+			})
+		},
+		getObjType: function (id) {
+			return ObjectTypes.findOne({
+				_id: id
+			});
+		},
+		getContactableTypes: function () {
+			return ObjectTypes.find({
+				type: Enums.objectTypeTypes.contactable
+			}).fetch();
+		},
+		getJobTypes: function () {
+			return ObjectTypes.find({
+				type: Enums.objectTypeTypes.job
+			}).fetch();
+		}
+	});
 });
