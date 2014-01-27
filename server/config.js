8ba5137ce1a35c18285d1e57ba5c7a69b3533d73
jberneche <javier@ubuntu.(none)>
2014-01-27 12:39:52 -0200

    deleting config

--- server/config.js
+++
@@ -1,9 +0,0 @@
-ExartuConfig = {
-    MAIL_URL: "smtp://postmaster%40sandbox93318.mailgun.org:2ufkeszxbxx7@smtp.mailgun.org:587",
-    SystemHierarchyId: "asdasd"
-}
-
-GoogleConfig = {
-    clientId: '34007040982.apps.googleusercontent.com',
-    clientSecret: 'C-eO4aMmGyOc6T7Esp-0bMob'
-}
