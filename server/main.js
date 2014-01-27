f6b7924a2587a3abdc9d69f464d5c91cbb530f82
jberneche <javier@ubuntu.(none)>
2014-01-27 12:32:21 -0200

    auto-config google accouts when server startup. the client secret and client id must be configured in GoogleConfig object (git-ignored)

--- server/main.js
+++ server/main.js
@@ -219,5 +219,24 @@ Meteor.startup(function () {
      */
     _.forEach(dbSeed, function (seedFn) {
         seedFn.call();
-    })
+    });
+    var accountsConfig = Accounts.loginServiceConfiguration._collection;
+    var googleConfig = accountsConfig.findOne({
+        "service": 'google'
+    });
+
+    if (!googleConfig) {
+        //read the config
+        if (!GoogleConfig) {
+            console.log('can not config google login, client\'s credential not found');
+
+        } else {
+            accountsConfig.insert({
+                service: "google",
+                clientId: GoogleConfig.clientId,
+                secret: GoogleConfig.clientSecret
+            });
+            console.log('google accounts configured successfully');
+        }
+    }
 });
