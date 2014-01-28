271c7a38552a2b8bd8dd160c18c47ed3d75959a4
jberneche <javier@ubuntu.(none)>
2014-01-28 18:30:19 -0200

    fix contactable add (misspelled enum)

--- client/js/addContactable.js
+++ client/js/addContactable.js
@@ -1,30 +1,31 @@
 Template.addContactable.viewmodel = function (objname) {
-	var self = this;
-	var myPerson = new koPerson();
-	var myOrg = new koOrganization();
-    var objType=ObjTypes.findOne({objName: objname});
+    var self = this;
+    var myPerson = new koPerson();
+    var myOrg = new koOrganization();
+    var objType = ObjTypes.findOne({
+        objName: objname
+    });
     self.objTypeName = ko.observable('');
-	self.ready = ko.observable(false);
+    self.ready = ko.observable(false);
     self.selectedType = ko.observable();
-    self.setSelectedType=function(val)
-    {
+    self.setSelectedType = function (val) {
         switch (val) {
-            case Enums.personType.human:
-                _.extend(self.contactable(), {
-                    person: myPerson
-                });
-                if (self.contactable().organization) {
-                    self.contactable().organization = null;
-                }
-                break;
-            case Enums.personType.organization:
-                _.extend(self.contactable(), {
-                    organization: myOrg
-                });
-                if (self.contactable().person) {
-                    self.contactable().person = null;
-                }
-                break;
+        case Enums.personType.human:
+            _.extend(self.contactable(), {
+                person: myPerson
+            });
+            if (self.contactable().organization) {
+                self.contactable().organization = null;
+            }
+            break;
+        case Enums.personType.organization:
+            _.extend(self.contactable(), {
+                organization: myOrg
+            });
+            if (self.contactable().person) {
+                self.contactable().person = null;
+            }
+            break;
         }
     };
     _.forEach(objType.fields, function (item) {
@@ -36,16 +37,18 @@ Template.addContactable.viewmodel = function (objname) {
                 }
             })
         });
-        if(item.fieldType==Enum.fieldType.lookUp){
+        if (item.fieldType == Enums.fieldType.lookUp) {
             _.extend(item, {
                 value: ko.observable(item.defaultValue),
-                options: LookUps.findOne({name: item.LookUpName}).items,            
+                options: LookUps.findOne({
+                    name: item.lookUpName
+                }).items,
             })
         }
     });
-	self.selectedType.subscribe(function (newval) {
+    self.selectedType.subscribe(function (newval) {
         self.setSelectedType(newval);
-	});
+    });
 
 
     self.objTypeName(objType.objName);
@@ -69,35 +72,35 @@ Template.addContactable.viewmodel = function (objname) {
 
     self.ready(true);
 
-	self.addContactable = function () {
-		if (!self.contactable.isValid()) {
-			self.contactable.errors.showAllMessages();
-			return;
-		};
-		var relNames = _.map(self.relations(), function (r) {
-			return r.relation.name;
-		});
-		var relValues = _.map(self.relations(), function (r) {
-			if (r.value()) return r.value()._id();
-		});
-		_.extend(self.contactable(), _.object(relNames, relValues));
+    self.addContactable = function () {
+        if (!self.contactable.isValid()) {
+            self.contactable.errors.showAllMessages();
+            return;
+        };
+        var relNames = _.map(self.relations(), function (r) {
+            return r.relation.name;
+        });
+        var relValues = _.map(self.relations(), function (r) {
+            if (r.value()) return r.value()._id();
+        });
+        _.extend(self.contactable(), _.object(relNames, relValues));
 
-		var fields = self.contactable()[self.objTypeName()]();
-		delete self.contactable()[self.objTypeName()];
-		self.contactable()[self.objTypeName()] = {};
-		_.forEach(fields, function (field) {
-			self.contactable()[self.objTypeName()][field.name] = field.value() || field.defaultValue;
-		})
-		Meteor.call('addContactable', ko.toJS(self.contactable), function (err, result) {
-			console.log(err);
-		});
-		$('#addContactableModal').modal('hide');
-	}
-	return this;
+        var fields = self.contactable()[self.objTypeName()]();
+        delete self.contactable()[self.objTypeName()];
+        self.contactable()[self.objTypeName()] = {};
+        _.forEach(fields, function (field) {
+            self.contactable()[self.objTypeName()][field.name] = field.value() || field.defaultValue;
+        })
+        Meteor.call('addContactable', ko.toJS(self.contactable), function (err, result) {
+            console.log(err);
+        });
+        $('#addContactableModal').modal('hide');
+    }
+    return this;
 }
 
 Meteor.methods({
-	addContactable: function (contactable) {
-		Contactables.insert(contactable);
-	}
+    addContactable: function (contactable) {
+        Contactables.insert(contactable);
+    }
 });
