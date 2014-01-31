283a689324088c32c06a085f8800c8507b989154
psoto <psoto23.ps@gmail.com>
2014-01-31 13:29:01 -0600

    Dummy configuration

---
+++ server/config.js
@@ -0,0 +1,8 @@
+ExartuConfig = {
+	// Insert mail server address
+	MAIL_URL: undefined,
+	SystemHierarchyId: "sys",
+	// Insert your google information
+	GoogleConfig_clientId: undefined,
+	GoogleConfig_clientSecret: undefined
+}
