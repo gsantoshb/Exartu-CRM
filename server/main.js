0c0b6141cfcabb88b26c132c6d9406be4e30ae80
psoto <psoto23.ps@gmail.com>
2014-01-31 13:30:59 -0600

    Fix google configuration check

--- server/main.js
+++ server/main.js
@@ -6,37 +6,32 @@ var dbSeed = {
 }
 
 Meteor.startup(function () {
-    /*
-     * Seed database
-     * Execute all function defined in seedSystemObjTypes
-     */
-    _.forEach(dbSeed, function (seedFn) {
-        seedFn.call();
-    });
-    var accountsConfig = Accounts.loginServiceConfiguration._collection;
-    var googleConfig = accountsConfig.findOne({
-        "service": 'google'
-    });
-    if (!ExartuConfig)
-    {
-        console.log('can not configure google login or smtp credentials because no Exartu config info is set up');
-    }
-    else
-    {
-    if (!googleConfig) {
-        //read the config
-        if (!ExartuConfig.GoogleConfig_clientId) {
-            console.log('can not config google login, client\'s credential not found');
+	/*
+	 * Seed database
+	 * Execute all function defined in seedSystemObjTypes
+	 */
+	_.forEach(dbSeed, function (seedFn) {
+		seedFn.call();
+	});
 
-        } else
-        {
-            accountsConfig.insert({
-                service: "google",
-                clientId: ExartuConfig.GoogleConfig_clientId,
-                secret: ExartuConfig.GoogleConfig_clientSecret
-            });
-            console.log('google accounts configured successfully');
-        }
-    }
-    }
+	var accountsConfig = Accounts.loginServiceConfiguration._collection;
+	var googleConfig = accountsConfig.findOne({
+		"service": 'google'
+	});
+	if (!ExartuConfig) {
+		console.log('can not configure google login or smtp credentials because no Exartu config info is set up');
+	} else {
+		//read the config
+		if (!ExartuConfig.GoogleConfig_clientId || !ExartuConfig.GoogleConfig_clientSecret) {
+			console.log('can not config google login, client\'s credential not found');
+
+		} else {
+			accountsConfig.insert({
+				service: "google",
+				clientId: ExartuConfig.GoogleConfig_clientId,
+				secret: ExartuConfig.GoogleConfig_clientSecret
+			});
+			console.log('google accounts configured successfully');
+		}
+	}
 });
