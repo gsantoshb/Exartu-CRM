5b5a76305892087b43f27a54bd6515a19af0a74a
gregg dourgarian <greggd@tempworks.com>
2014-01-30 20:25:01 -0600

    allow the system to run even without google or exartu config info
    had to drop back to prior versions of user.js and hierarchy.js because
    looks like work in progress

--- server/collections/hierarchy.js
+++ server/collections/hierarchy.js
@@ -1,74 +1,26 @@
+createHier = function (name, parent) {
+    return Meteor.call('createHier', {
+        name: name,
+        parent: parent
+    });
+}
 Meteor.startup(function () {
-	Meteor.methods({
-		createHier: function (hier) {
-			//TODO: Check options values
+    Meteor.methods({
+        createHier: function (options) {
+            //TODO: Check options values
 
-			if (hier.parent != null) {
-				console.log('New hier with parent');
-				var parentHier = Hierarchies.findOne({
-					_id: hier.parent
-				});
-				if (parentHier == null)
-					throw new Meteor.Error(404, "Parent hierarchy does not exist");
+            if (options.parent != null) {
+                var parentHier = Hierarchies.findOne({
+                    _id: options.parent
+                }).fetch();
+                if (parentHier == null)
+                    throw new Meteor.Error(404, "Parent hierarchy does not exist");
+            }
 
-				hier._id = generateUniqueHierId(hier.parent);
-
-			} else {
-				console.log('New hier without parent');
-				hier._id = generateUniqueHierId(ExartuConfig.SystemHierarchyId);
-			}
-
-			Hierarchies.insert(hier);
-
-			return hier._id;
-		},
-		testHierSystem: function () {
-			// Test: Create a child hier
-			var parentId = Meteor.call('createHier', {
-				name: 'parent',
-			});
-			console.log(parentId);
-			var childId = Meteor.call('createHier', {
-				name: 'child',
-				parent: parentId
-			});
-			console.log(childId);
-
-			//			var asd = Meteor.call('addHierUser', childId, {
-			//				name: 'child_user',
-			//				email: 'child_user@asd.com',
-			//				password: 'asdasd'
-			//			});
-			console.log('Relation test')
-
-			if (getHierarchiesRelation(parentId, childId) == 1)
-				console.log('Relation 1: OK');
-			else
-				console.log('Relation 1: FAIL');
-
-			if (getHierarchiesRelation(childId, parentId) == -1)
-				console.log('Relation 2: OK');
-			else
-				console.log('Relation 2: FAIL');
-
-			if (getHierarchiesRelation(parentId, parentId) == -1)
-				console.log('Relation 3: OK');
-			else
-				console.log('Relation 3: FAIL');
-		}
-	});
+            return Hierarchies.insert({
+                name: options.name,
+                parent: options.parent
+            });
+        }
+    });
 });
-
-var generateUniqueHierId = function (prefix) {
-	var suffixId = (1 + Math.random()).toString(36).substr(2, 3);
-	//console.log(prefix);
-	var candidate = prefix != undefined ? prefix + '-' + suffixId : suffixId;
-
-	if (Hierarchies.findOne({
-		_id: candidate
-	}) == null) {
-		return candidate;
-	} else {
-		generateUniqueHierId(prefix);
-	}
-};
--- server/collections/user.js
+++ server/collections/user.js
@@ -1,64 +1,46 @@
 Accounts.onCreateUser(function (options, user) {
-	//console.dir(options);
-	//console.dir(user);
-
-	var userEmail = user.email;
-	if (user.services) {
-		if (user.services.google) {
-			//todo: check if the account is already in the database
-			userEmail = user.services.google.email;
-			user.emails = [{
-				"address": userEmail,
-				"verified": true
+    console.dir(options);
+    console.dir(user);
+    if (user.services) {
+        if (user.services.google) {
+            //todo: check if the account is already in the database
+            var email = user.services.google.email;
+            options.hierId = createHier(email.split('@')[0]);
+            user.emails = [{
+                "address": email,
+                "verified": true
             }];
+        }
+    }
+    if (options.hierId == null) {
+        options.hierId = createHier(options.email.split('@')[0]);
+    }
 
-		}
-	}
-	if (options.profile.hierId == null) {
-		options.profile.hierId = Meteor.call('createHier', {
-			name: userEmail.split('@')[0]
-		});
-	}
+    user.hierId = options.hierId;
+    user._id = Random.id();
+    Hierarchies.update({
+        _id: user.hierId
+    }, {
+        $addToSet: {
+            users: user._id
+        }
+    });
 
-	user.hierId = options.profile.hierId;
-	user._id = Random.id();
-	Hierarchies.update({
-		_id: user.hierId
-	}, {
-		$addToSet: {
-			users: user._id
-		}
-	});
-
-	return user;
+    return user;
 });
 
 /*
  * extending the user data that is sended to the client
  */
 Meteor.publish("userData", function () {
-	return Meteor.users.find({
-		_id: this.userId
-	}, {
-		fields: {
-			'emails': 1,
-			'emails': 1,
-			'services.google.picture': 1,
-			"hierId": 1
-		}
-	});
-});
-
-Meteor.methods({
-	addHierUser: function (hierId, user) {
-		var options = {};
-		options.username = user.name;
-		options.email = user.email;
-		options.password = user.password;
-		options.profile = {
-			hierId: hierId,
-			// more information from user
-		}
-		return Accounts.createUser(options);
-	}
+    return Meteor.users.find({
+        _id: this.userId
+    }, {
+        fields: {
+            'emails': 1,
+            'emails': 1,
+            'services.google.picture': 1,
+            "hierId": 1
+        }
+    });
 });
