c43b60ba02dd94cb7f3c733af27644d43273cf38
jberneche <javier@ubuntu.(none)>
2014-02-11 15:53:49 -0200

    cleanup in contactables: there was many repeated code

--- client/js/contactables.js
+++ client/js/contactables.js
@@ -25,70 +25,55 @@ ContactablesController = RouteController.extend({
     },
 });
 Template.contactables.waitOn = 'ContactableHandler';
+
 Template.contactables.viewModel = function () {
     //    debugger;
     var self = {};
     self.ready = ko.observable(false);
-    self.objTypeSelected = ko.observable(objType());
-
-    var entitiesQuery = {};
-    if (self.objTypeSelected())
-        entitiesQuery = {
-            objNameArray: {
-                $in: [self.objTypeSelected().objName]
-            }
-        }
-    objType.subscribe(function (newValue) {
-        //        debugger;
-        self.objTypeSelected(objType());
-
-        entitiesQuery = {};
-        if (self.objTypeSelected())
+    self.entities = ko.observableArray();
+    self.contactableTypes = ko.observableArray();
+    self.objName = ko.observable('Contactables');
+    var selectObjType = function (newValue) {
+        var entitiesQuery = {};
+        var filter = {
+            objGroupType: Enums.objGroupType.contactable
+        };
+        if (newValue) {
             entitiesQuery = {
                 objNameArray: {
-                    $in: [self.objTypeSelected().objName]
+                    $in: [newValue.objName]
                 }
             }
-        self.entities(ko.mapping.fromJS(Contactables.find(entitiesQuery).fetch())());
+            self.entities(ko.mapping.fromJS(Contactables.find(entitiesQuery).fetch())());
+            filter.objName = newValue.objName;
+            self.objName(newValue.objName)
+        } else {
+            self.objName('Contactables')
+        }
 
-        var filter = {
-            objGroupType: Enums.objGroupType.contactable
-        };
-        if (self.objTypeSelected() != undefined)
-            filter.objName = self.objTypeSelected().objName;
-
-        var result = ObjTypes.find(filter).fetch();
-
-        self.contactableTypes(result);
-    });
-    self.entities = ko.meteor.find(Contactables, entitiesQuery);
-
-    self.contactableTypes = ko.observableArray();
-
-    Meteor.subscribe('objTypes', function () {
-        self.getIconForObjName = function (objname) {
-            var type = ObjTypes.findOne({
-                objName: objname
-            });
-            return 'glyphicon ' + type.glyphicon;
-        };
-
-        var filter = {
-            objGroupType: Enums.objGroupType.contactable
-        };
-        if (self.objTypeSelected != undefined)
-            filter.objName = self.objTypeSelected.objName;
 
         var result = ObjTypes.find(filter).fetch();
 
         self.contactableTypes(result);
-        _.extend(self, helper.createObjTypefilter(['person.firstName', 'person.lastName', 'organization.organizationName'], result,
-            function () {
-                console.log('query',this.query);
-                self.entities(ko.mapping.fromJS(Contactables.find(this.query).fetch())());
-            }));
-        self.ready(true);
-    });
+
+    }
+    selectObjType(objType());
+
+    objType.subscribe(selectObjType);
+
+    self.getIconForObjName = function (objname) {
+        var type = ObjTypes.findOne({
+            objName: objname
+        });
+        return 'glyphicon ' + type.glyphicon;
+    };
+    //    _.extend(self, helper.createObjTypefilter(['person.firstName', 'person.lastName', 'organization.organizationName'], result,
+    //        function () {
+    //            console.log('query', this.query);
+    //            self.entities(ko.mapping.fromJS(Contactables.find(this.query).fetch())());
+    //        }));
+
+    self.ready(true);
 
     self.showAddContactableModal = function (typeId) {
         Session.set('newContactableTypeId', typeId);
