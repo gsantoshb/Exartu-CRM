deec476d9c37dc59f250030d31afdac155026224
jberneche <javier@ubuntu.(none)>
2014-02-20 16:26:07 -0200

    delete configuration, it shouldn't be in the repo

--- server/config.js
+++ server/config.js
@@ -1,9 +1,8 @@
-
-
-ExartuConfig =
-{
-    MAIL_URL    :   "smtp://postmaster%40sandbox93318.mailgun.org:2ufkeszxbxx7@smtp.mailgun.org:587",
-    SystemHierarchyId: "asdasd",
-    GoogleConfig_clientId: '34007040982.apps.googleusercontent.com',
-    GoogleConfig_clientSecret: 'C-eO4aMmGyOc6T7Esp-0bMob'
-};
+ExartuConfig = {
+    // Insert mail server address
+    MAIL_URL: undefined,
+    SystemHierarchyId: "sys",
+    // Insert your google information
+    GoogleConfig_clientId: undefined,
+    GoogleConfig_clientSecret: undefined
+}
