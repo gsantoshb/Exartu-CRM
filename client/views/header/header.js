03742e1b5e9dd0021600c5684daf97502d437f0c
jberneche <javier@ubuntu.(none)>
2014-02-20 14:04:40 -0200

    fix previous commit, change the click bind, remove the '.submenus > li' logic, remove event's preventDefault to let the navigation work properly

--- client/views/header/header.js
+++ client/views/header/header.js
@@ -142,8 +142,8 @@ Template.header.rendered = function () {
         placement: 'bottom'
     });
 
-    $(document).on('click', '.submenus > li', function (e) {
-        e.preventDefault();
+    var submenuLogic = function (e) {
+        //        e.preventDefault();
         var submenu = $(this).siblings('ul');
         var li = $(this).parents('li');
         if ($(window).width() > 480) {
@@ -173,42 +173,10 @@ Template.header.rendered = function () {
             li.addClass('open');
         }
         $('html').getNiceScroll().resize();
-    });
+    };
 
-    var submenuLink = $('.submenu > a');
-
-    submenuLink.unbind("click").click(function (e) {
-        e.preventDefault();
-        var submenu = $(this).siblings('ul');
-        var li = $(this).parents('li');
-        if ($(window).width() > 480) {
-            var submenus = $('#sidebar li.submenu ul');
-            var submenus_parents = $('#sidebar li.submenu');
-        } else {
-            var submenus = $('#jPanelMenu-menu li.submenu ul');
-            var submenus_parents = $('#jPanelMenu-menu li.submenu');
-        }
-
-        if (li.hasClass('open')) {
-            if (($(window).width() > 768) || ($(window).width() <= 480)) {
-                submenu.slideUp();
-            } else {
-                submenu.fadeOut(250);
-            }
-            li.removeClass('open');
-        } else {
-            if (($(window).width() > 768) || ($(window).width() <= 480)) {
-                submenus.slideUp();
-                submenu.slideDown();
-            } else {
-                submenus.fadeOut(250);
-                submenu.fadeIn(250);
-            }
-            submenus_parents.removeClass('open');
-            li.addClass('open');
-        }
-        $('html').getNiceScroll().resize();
-    });
+    console.dir($('.submenu > a'));
+    $('.submenu > a').on('click', submenuLogic);
 
     $('.go-full-screen').click(function () {
         backdrop = $('.white-backdrop');
