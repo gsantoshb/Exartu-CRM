105d12b56dc256b773f92855516d6c714a788786
psoto <psoto23.ps@gmail.com>
2014-04-17 17:29:11 -0300

    Read APM secret from env

--- server/main.js
+++ server/main.js
@@ -47,5 +47,6 @@ Meteor.startup(function () {
   }
 
   var appId = process.env['APM_ID'] || 'vy8mpCMHy6r3RrByb';
-  Apm.connect(appId, 'dfe473b6-b92b-4848-862d-120bff294695');
+  var secret = process.env['APM_SECRET'] || 'dfe473b6-b92b-4848-862d-120bff294695';
+  Apm.connect(appId, secret);
 });
