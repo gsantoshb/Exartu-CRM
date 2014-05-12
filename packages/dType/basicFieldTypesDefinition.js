af6f890c4cd0663022d835abaee278a0fbad079b
jberneche <javier@ubuntu.(none)>
2014-05-12 15:53:29 -0300

    change the field types definition to return an error mesage

--- packages/dType/basicFieldTypesDefinition.js
+++ packages/dType/basicFieldTypesDefinition.js
@@ -1,34 +1,61 @@
 // basic FieldTypes
 dType.core.createFieldType({
     name: 'string',
-    validate: function(value, fieldDefinition){
-        if (!_.isString(value))
+    validate: function(value, fieldDefinition, error){
+        var error=error||{};
+        if (fieldDefinition.required && !value){
+            error.message='this field is required';
             return false;
-
-        if(!fieldDefinition.regex)
+        }
+        if (!_.isString(value)){
+            return false;
+        }
+        if(!fieldDefinition.regex){
             return true;
-
-        return new RegExp(fieldDefinition.regex).test(value);
+        }
+        if (! new RegExp(fieldDefinition.regex).test(value)){
+            error.message='Check this value';
+            return false;
+        }
+        return true;
     },
     defaultValue: ''
 })
 dType.core.createFieldType({
     name: 'number',
-    validate: function(value, fieldDefinition){
-        if (!_.isNumber(value))
+    validate: function(value, fieldDefinition, error){
+        var error=error||{};
+        if (fieldDefinition.required && !value){
+            error.message='this field is required';
+            return false;
+        }
+        if (!_.isNumber(value)){
+            error.message='invalid value';
             return false
+        }
         if (!fieldDefinition.range){
             return true
         }
-        return fieldDefinition.range.min >= value && fieldDefinition.range.min >= value;
+        if(!fieldDefinition.range.min >= value && fieldDefinition.range.min >= value){
+            error.message='value out of range';
+            return false
+        };
+        return true
     },
     defaultValue: 0
 })
 dType.core.createFieldType({
     name: 'date',
-    validate: function(value, fieldDefinition){
-        if (!_.isDate(value))
+    validate: function(value, fieldDefinition, error){
+        var error=error||{};
+        if (fieldDefinition.required && !value){
+            error.message='this field is required';
             return false;
+        }
+        if (!_.isDate(value)){
+            error.message='invalid value';
+            return false;
+        }
         return true
 
     },
@@ -36,18 +63,36 @@ dType.core.createFieldType({
 })
 dType.core.createFieldType({
     name: 'enum',
-    validate: function(value, fieldDefinition){
+    validate: function(value, fieldDefinition, error){
+        var error=error||{};
+        if (fieldDefinition.required && !value){
+            error.message='this field is required';
+            return false;
+        }
         if (!fieldDefinition.options){
             return true
         }
-        return _.contains(fieldDefinition.options, value);
+        if(_.contains(fieldDefinition.options, value)){
+            error.message='invalid value';
+            return false;
+        }
+        return true;
     },
     defaultValue: null
 })
 dType.core.createFieldType({
     name: 'boolean',
-    validate: function(value, fieldDefinition){
-        return _.isBoolean(value)
+    validate: function(value, fieldDefinition, error){
+        var error=error||{};
+        if (fieldDefinition.required && !value){
+            error.message='this field is required';
+            return false;
+        }
+        if(!_.isBoolean(value)){
+            error.message='invalid value';
+            return false;
+        }
+        return true;
     },
     defaultValue: true
 })
