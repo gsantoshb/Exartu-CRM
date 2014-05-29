4461e9e7ff9472f5a1a26842a02bafa717486976
psoto <psoto23.ps@gmail.com>
2014-05-29 14:25:18 -0300

    Allow assignment update on client side and clear reference when an assignment is setted as inactive

--- server/collections/assignment.js
+++ server/collections/assignment.js
@@ -1,171 +1,123 @@
 Meteor.publish('assignment', function () {
 
-    if (!this.userId)
-        return false;
+  if (!this.userId)
+    return false;
 
-    return Assignment.find();
+  return Assignment.find();
 });
+
+Assignment.allow({
+  insert: function (userId, doc) {
+    return true;
+  },
+  update: function (userId, doc, fields, modifier) {
+    return Meteor.user() && methods.getHierarchiesRelation(Meteor.user().hierId, doc.hierId) == -1;
+  },
+  remove: function (userId, doc) {
+    return false;
+  }
+})
 
 
 //<editor-fold desc="************ update job and contactable ****************">
-Assignment.before.insert(function(userId, doc){
-    if(!doc._id){
-        doc._id=Meteor.uuid();
+Assignment.before.insert(function (userId, doc) {
+  doc.hierId = Meteor.user().hierId;
+
+  if (!doc._id) {
+    doc._id = Meteor.uuid();
+  }
+  Contactables.update({
+    _id: doc.employee
+  }, {
+    $set: {
+      jobAssigned: doc.job,
+      assignment: doc._id
     }
-    Contactables.update({
-        _id: doc.employee
-    }, {
-        $set: {
-            jobAssigned: doc.job,
-            assignment: doc._id
-        }
-    });
-    Jobs.update({
-        _id: doc.job
-    }, {
-        $set: {
-            employeeAssigned: doc.employee,
-            assignment: doc._id
-        }
-    });
+  });
+  Jobs.update({
+    _id: doc.job
+  }, {
+    $set: {
+      employeeAssigned: doc.employee,
+      assignment: doc._id
+    }
+  });
 })
+
 //before set the employee and job in null becouse they may change
-Assignment.before.update(function(userId, doc, fieldNames, modifier, options){
-
-    if (modifier && modifier.$set && modifier.$set.employee && (modifier.$set.employee != doc.employee)){
-       Contactables.update({
-            _id: doc.employee
-        }, {
-            $set: {
-                jobAssigned: null,
-                assignment: null
-            }
-        });
-    }
-//        Jobs.update({
-//            _id: doc.job
-//        }, {
-//            $set: {
-//                employeeAssigned: null,
-//                assignment: null
-//            }
-//        });
-
-});
-//after update the employee and job
-Assignment.after.update(function(userId, doc, fieldNames, modifier, options){
+Assignment.before.update(function (userId, doc, fieldNames, modifier, options) {
+  if (modifier && modifier.$set &&
+    (modifier.$set.employee && (modifier.$set.employee != doc.employee) ||
+      (modifier.$set.inactive))) {
     Contactables.update({
-        _id: doc.employee
+      _id: doc.employee
     }, {
-        $set: {
-            jobAssigned: doc.job,
-            assignment: doc._id
-        }
+      $set: {
+        jobAssigned: null,
+        assignment: null
+      }
     });
-    Jobs.update({
+
+    if (modifier.$set.inactive)
+      Jobs.update({
         _id: doc.job
-    }, {
+      }, {
         $set: {
-            employeeAssigned: doc.employee,
-            assignment: doc._id
+          employeeAssigned: null,
+          assignment: null
         }
-    });
+      });
+  }
+});
+
+//after update the employee and job
+Assignment.after.update(function (userId, doc, fieldNames, modifier, options) {
+  Contactables.update({
+    _id: doc.employee
+  }, {
+    $set: {
+      jobAssigned: doc.job,
+      assignment: doc._id
+    }
+  });
+  Jobs.update({
+    _id: doc.job
+  }, {
+    $set: {
+      employeeAssigned: doc.employee,
+      assignment: doc._id
+    }
+  });
 });
 //</editor-fold>
 
-//<editor-fold desc="************ update job and contactable ****************">
-Assignment.before.insert(function(userId, doc){
-    if(!doc._id){
-        doc._id=Meteor.uuid();
+Meteor.startup(function () {
+  createAssignment = function (job, employee, assignmentInfo) {
+    if (!job || !employee) {
+      throw new Error('parameters must not be null')
     }
-    Contactables.update({
-        _id: doc.employee
-    }, {
-        $set: {
-            jobAssigned: doc.job,
-            assignment: doc._id
-        }
-    });
-    Jobs.update({
-        _id: doc.job
-    }, {
-        $set: {
-            employeeAssigned: doc.employee,
-            assignment: doc._id
-        }
-    });
-})
-//before set the employee and job in null because they may change
-Assignment.before.update(function(userId, doc, fieldNames, modifier, options){
+    assignmentInfo = assignmentInfo || {};
 
-    if (modifier && modifier.$set && modifier.$set.employee && (modifier.$set.employee != doc.employee)){
-        Contactables.update({
-            _id: doc.employee
-        }, {
-            $set: {
-                jobAssigned: null,
-                assignment: null
-            }
-        });
+    var assignment = {
+      employee: employee._id,
+      job: job._id,
+      jobType: job.objNameArray[0]
     }
-//        Jobs.update({
-//            _id: doc.job
-//        }, {
-//            $set: {
-//                employeeAssigned: null,
-//                assignment: null
-//            }
-//        });
+    assignment.start = assignmentInfo.start ? assignmentInfo.start : new Date();
+    assignment.end = assignmentInfo.end ? assignmentInfo.end : null;
+    assignment.rates = createRates(job, assignmentInfo);
+    assignment.qualification = null;
 
-});
-//after update the employee and job
-Assignment.after.update(function(userId, doc, fieldNames, modifier, options){
-    Contactables.update({
-        _id: doc.employee
-    }, {
-        $set: {
-            jobAssigned: doc.job,
-            assignment: doc._id
-        }
-    });
-    Jobs.update({
-        _id: doc.job
-    }, {
-        $set: {
-            employeeAssigned: doc.employee,
-            assignment: doc._id
-        }
-    });
-});
-//</editor-fold>
+    return Assignment.insert(assignment);
+  };
 
-Meteor.startup(function(){
-    createAssignment= function(job, employee, assignmentInfo){
-        if (!job||!employee){
-            throw new Error('parameters must not be null')
-        }
-        assignmentInfo= assignmentInfo || {};
-
-        var assignment={
-            employee: employee._id,
-            job: job._id,
-            jobType: job.objNameArray[0]
-        }
-        assignment.start= assignmentInfo.start ? assignmentInfo.start : new Date();
-        assignment.end= assignmentInfo.end ? assignmentInfo.end : null;
-        assignment.rates= createRates(job, assignmentInfo);
-        assignment.qualification= null;
-
-        return Assignment.insert(assignment);
+  createRates = function (job, assignmentInfo) {
+    var payRatesInfo = assignmentInfo.payRate || {};
+    var billRatesInfo = assignmentInfo.billRate || {};
+    var rate = {
+      payRate: payRatesInfo,
+      billRate: billRatesInfo
     };
-
-    createRates= function(job, assignmentInfo){
-        var payRatesInfo= assignmentInfo.payRate || {};
-        var billRatesInfo= assignmentInfo.billRate || {};
-        var rate= {
-            payRate:payRatesInfo,
-            billRate:billRatesInfo
-        };
 //        if (job.Temporary){
 //
 //            var frequency = LookUps.find({ codeType: Enums.lookUpTypes.payRate.frequencies.code, code: job.Temporary.frequency });
@@ -185,6 +137,6 @@ Meteor.startup(function(){
 //                rate.payRate.salary = job['Direct Hire'].salary;
 //            }
 //        }
-        return rate;
-    }
+    return rate;
+  }
 })
