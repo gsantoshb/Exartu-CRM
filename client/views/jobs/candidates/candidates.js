48dc3f7116043a1b11f244dc6291a1c7b8850181
jberneche <javier@ubuntu.(none)>
2014-06-03 16:28:15 -0300

    assign a candidate fix

--- client/views/jobs/candidates/candidates.js
+++ client/views/jobs/candidates/candidates.js
@@ -13,10 +13,14 @@ Template.candidates.helpers({
   },
   employeeInfo:function(candidateObject){
     return Contactables.findOne({_id: candidateObject.employee});
-  }
+  },
+
 })
 Template.candidates.events({
   'click .addEditCandidate':function () {
     Composer.showModal( 'candidateAdd',Session.get('entityId'));
+  },
+  'click .assign': function(){
+    Composer.showModal('assignmentAdd', Session.get('entityId'), this._id);
   }
 })
