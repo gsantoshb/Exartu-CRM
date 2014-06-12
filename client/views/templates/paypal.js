38f957e2970e7f70bbb2318aa58b1b65a2c4181c
jberneche <javier@ubuntu.(none)>
2014-06-12 13:26:29 -0300

    paypal buttons for release

--- client/views/templates/paypal.js
+++ client/views/templates/paypal.js
@@ -1,5 +1,22 @@
-Template.paypal.helpers({
-  hierId:function(){
-    return Meteor.user().hierId;
+UI.registerHelper('paypalButton', function(){
+
+  //todo: find a better way to do this
+  var templateName= 'paypal';
+
+  //mode
+  if (window.location.hostname=='crm.exartu.com' || window.location.hostname=='exartucrm.herokuapp.com/'){
+    //nothing
   }
-});
+  else{
+    templateName += 'Test';
+  }
+  //button type
+  templateName += (this.type || 'Enterprise');
+
+  Template[templateName].helpers({
+    hierId: function(){
+      return Meteor.user().hierId;
+    }
+  });
+  return Template[templateName]
+})
