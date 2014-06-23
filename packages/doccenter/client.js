55c2d69a0e6ed299a181821896e06e36ccd16278
psoto <psoto23.ps@gmail.com>
2014-06-23 16:16:57 -0300

    Remove doccenter package

--- packages/doccenter/client.js
+++
@@ -1,4 +0,0 @@
-Meteor.subscribe('doccenter_documents');
-Meteor.subscribe('doccenter_revisions');
-Meteor.subscribe('doccenter_revisionInstances');
-Meteor.subscribe('doccenter_pageImages');
--- packages/doccenter/tests/constructors.js
+++
@@ -1,4 +0,0 @@
-Tinytest.add("Constructor - Document and revisions", function (test, next) {
-  var doc = new DocCenter.Document();
-  console.dir(doc);
-});
