453adb121833eb84a8324ef615db3efe863c3c0f
javiber <javier.berneche@gmail.com>
2014-07-28 17:58:25 -0300

    add logo for HRCenter

--- client/lib/_subscriptions/hierarchies.js
+++ client/lib/_subscriptions/hierarchies.js
@@ -1,2 +1,7 @@
 Hierarchies = new Meteor.Collection("hierarchies");
 HierarchiesHandler = Meteor.subscribe('hierarchies');
+
+HierarchiesFS = new Document.Collection({
+  collection: Hierarchies
+});
+Meteor.subscribe(HierarchiesFS.collectionName);
