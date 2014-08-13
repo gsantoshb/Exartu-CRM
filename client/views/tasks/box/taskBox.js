ad2a9d141e17f26d99c01fefde1abd9df1cdee92
javiber <javier.berneche@gmail.com>
2014-08-13 15:14:16 -0300

    migrate task list to blaze remove complete button

--- client/views/tasks/box/taskBox.js
+++ client/views/tasks/box/taskBox.js
@@ -16,43 +16,14 @@ Template.taskBox.helpers({
   },
   tasks: function(){
     return Tasks.find({ links: { $elemMatch: { id: Session.get('entityId') } } }, { sort: { dateCreated: -1 } });
-  },
-  taskClass: function(){
-    return 'task-' + this.state;
-  },
-  getHref: function(){
-    return Utils.getHrefFromLink(this);
-  },
-  getEntity: function(){
-    return Utils.getEntityFromLink(this);
-  },
-  isMe: function(){
-    return (Meteor.userId() == this._id) ? 'text-info' : '';
   }
 })
 
 Template.taskBox.events({
-  'click .editTask': function(){
-    Composer.showModal('addEditTask', this)
-  },
   'click .addTask': function(){
     Composer.showModal('addEditTask', { links: [{
       id: Session.get('entityId'),
       type: entityType
     }] })
-  }
-})
-Template.taskState.helpers({
-  canComplete: function(){
-    return this.state == Enums.taskState.pending && _.contains(this.assign,Meteor.userId());
-  },
-  getStateIcon: function(){
-    return helper.getTaskStateIcon(this.state);
-  }
-})
-
-Template.showTaskDate.helpers({
-  statusIs: function(statusName){
-    return this.state == Enums.taskState[statusName];
   }
 })
