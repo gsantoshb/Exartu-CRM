b1a9ba9dc52c07b65f227b667439f13aa7884b7d
psoto <psoto23.ps@gmail.com>
2014-08-14 12:29:43 -0300

    New ES sync collection definition

--- common/esCollections.js
+++ common/esCollections.js
@@ -1,11 +1,21 @@
 // Contactables
-ES.syncCollection({
-	collection: Contactables,
-	fields: ['person.firstName', 'person.lastName']
-});
+// ES.syncCollection({
+// 	collection: Contactables,
+// 	fields: ['person.firstName', 'person.lastName']
+// });
 
 // Jobs
 ES.syncCollection({
 	collection: Jobs,
-	fields: ['publicJobTitle', 'tags']
+	fields: [
+		{ name: 'publicJobTitle', sugguster: true },
+		'tags'
+	]
+});
+
+ES.syncCollection({
+	collection: Notes,
+	fields: [
+		{ name: 'content', sugguster: true },
+	]
 });
