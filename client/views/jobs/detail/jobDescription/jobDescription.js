fcca39ff082147d428e7090477a8e80e6d50e0c5
Gregg Dourgarian <greggd@tempworks.com>
2014-08-17 16:29:10 -0500

    begin modularizing the Job template ... separate out the JobDescription template

---
+++ client/views/jobs/detail/jobDescription/jobDescription.js
@@ -0,0 +1,83 @@
+
+var generateReactiveObject = function(job) {
+  return new dType.objInstance(job, Jobs);
+};
+
+Template.jobDescription.created=function(){
+  self.editMode=false;
+  var originalJob=Jobs.findOne({ _id: Session.get('entityId') });
+
+
+  var definition={
+    reactiveProps:{
+      tags:{
+        default: originalJob.tags,
+        update: 'tags',
+        type: Utils.ReactivePropertyTypes.array
+      }
+    }
+  };
+  services= Utils.ObjectDefinition(definition);
+}
+var job;
+
+
+Template.jobDescription.helpers({
+  getType: function(){
+    return Enums.linkTypes.job;
+  }
+})
+
+// Job description
+var jobDescriptionEditMode = false;
+var jobDescriptionEditModeDep = new Deps.Dependency;
+var descriptionSelf={}
+Utils.reactiveProp(descriptionSelf, 'previewMode', false);
+
+Template.jobDescription.colorPreviewMode= function(){
+  return descriptionSelf.previewMode ? '#008DFC' : '#ddd'
+}
+
+Template.jobDescription.previewMode= function(){
+  return descriptionSelf.previewMode;
+}
+
+Template.jobDescription.editMode = function() {
+  jobDescriptionEditModeDep.depend();
+  return jobDescriptionEditMode;
+};
+
+Template.jobDescription.colorDescriptionEdit = function() {
+  jobDescriptionEditModeDep.depend();
+  return jobDescriptionEditMode ? '#008DFC' : '#ddd';
+};
+
+Template.jobDescription.events = {
+  'click .editJobDescription': function(){
+    jobDescriptionEditMode = !jobDescriptionEditMode;
+    jobDescriptionEditModeDep.changed();
+  },
+  'click #cancelJobDescriptionEdit':function(){
+    jobDescriptionEditMode = false;
+    jobDescriptionEditModeDep.changed();
+  },
+  'click #saveJobDescriptionEdit':function() {
+    var update = job.getUpdate();
+    if (!update.$set || !update.$set.jobDescription) {
+      jobDescriptionEditMode = false;
+      jobDescriptionEditModeDep.changed();
+      return; // Nothing to update
+    }
+
+    Jobs.update({_id: job._id}, {$set: { jobDescription: update.$set.jobDescription }}, function(err, result) {
+      if (!err) {
+        jobDescriptionEditMode = false;
+        jobDescriptionEditModeDep.changed();
+        job.jobDescription.defaultValue = job.jobDescription.value; // Reset jobDescription initial value
+      }
+    });
+  },
+  'click .previewMode': function(){
+    descriptionSelf.previewMode= ! descriptionSelf.previewMode;
+  }
+};
