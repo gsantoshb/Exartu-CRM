709993915fd3bea85a882d4d67420c1a252b2135
psoto <psoto23.ps@gmail.com>
2014-08-18 16:08:28 -0300

    Add Elasticsearch relations to contactables

--- common/esCollections.js
+++ common/esCollections.js
@@ -1,21 +1,22 @@
 // Contactables
-// ES.syncCollection({
-// 	collection: Contactables,
-// 	fields: ['person.firstName', 'person.lastName']
-// });
-
-// Jobs
 ES.syncCollection({
-	collection: Jobs,
+	collection: Contactables,
 	fields: [
-		{ name: 'publicJobTitle', sugguster: true },
-		'tags'
-	]
-});
-
-ES.syncCollection({
-	collection: Notes,
-	fields: [
-		{ name: 'content', sugguster: true },
+		'person.firstName', 'person.lastName', 'person.middleName', 'person.jobTitle',
+		'organization.organizationName',
+		'Customer.department',
+		'Contact.statusNote',
+		'Employee.statusNote',
+		'Employee.recruiterStatus',
+		'tags', 'contactMethods.value', 'location.displayName', 'notes.value, resume.value'
+	],
+	// Retrive data related to items on this collections
+	// If idFiled is defined then its a "inverse relation"
+	relations: [{
+			idField: 'links.id',
+			fieldName: 'notes',
+			valuePath: 'content',
+			collection:	Notes
+		}
 	]
 });
