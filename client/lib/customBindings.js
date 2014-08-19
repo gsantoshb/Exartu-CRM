4f525e128da60671b1568cff3bf32c581af8b826
Gregg Dourgarian <greggd@tempworks.com>
2014-08-19 07:52:45 -0500

    neaten up the filter area; improve jobs list display

--- client/lib/customBindings.js
+++ client/lib/customBindings.js
@@ -236,7 +236,12 @@ ko.bindingHandlers.select2 = {
     });
   }
 };
-
+ko.bindingHandlers.placeholder = {
+  init: function (element, valueAccessor, allBindingsAccessor) {
+    var underlyingObservable = valueAccessor();
+    ko.applyBindingsToNode(element, { attr: { placeholder: underlyingObservable } } );
+  }
+};
 
 // Register new rules
 ko.validation.registerExtenders();
