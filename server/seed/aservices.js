dd4c70bf9fb4600981f1e290086c15aa8a80c647
Gregg Dourgarian <greggd@tempworks.com>
2014-08-25 07:44:52 -0500

    job rate fix

--- server/seed/aservices.js
+++ server/seed/aservices.js
@@ -108,38 +108,39 @@ dType.constructor.service({
     },
     initValue: function(value, serviceSettings, type, obj){
       var result=[];
-      if (obj['Temporary']){
-        var frequency = LookUps.findOne({ codeType: Enums.lookUpTypes.payRate.frequencies.code, _id: obj.Temporary.frequency });
-        if(! frequency) return [];
-
-        var pay= (obj.Temporary.pay / frequency.inHours);
-        var bill= (obj.Temporary.bill / frequency.inHours);
-//        var bill=pay+pay*(obj.fee/100);
-
-        var rateType = JobRateTypes.findOne({displayName:'RegularTime'});
-        if (!_.findWhere(value, { type: rateType._id })){
-          result.push({ type: rateType._id, pay: pay, bill: bill });
-        }
-        //<editor-fold desc="other rates">
-        //        rateType = JobRateTypes.findOne({displayName:'Over Time'});
+      // this code commented out because is creating jobrates with bad typeid...using the text display name not reliable anyway
+//      if (obj['Temporary']){
+//        var frequency = LookUps.findOne({ codeType: Enums.lookUpTypes.payRate.frequencies.code, _id: obj.Temporary.frequency });
+//        if(! frequency) return [];
+//
+//        var pay= (obj.Temporary.pay / frequency.inHours);
+//        var bill= (obj.Temporary.bill / frequency.inHours);
+////        var bill=pay+pay*(obj.fee/100);
+//
+//        var rateType = JobRateTypes.findOne({displayName:'RegularTime'});
 //        if (!_.findWhere(value, { type: rateType._id })){
-//          result.push({ type: rateType._id, pay: pay*1.5, bill: bill*1.5 });
+//          result.push({ type: rateType._id, pay: pay, bill: bill });
 //        }
-//        rateType = JobRateTypes.findOne({displayName:'Double Time'});
+//        //<editor-fold desc="other rates">
+//        //        rateType = JobRateTypes.findOne({displayName:'Over Time'});
+////        if (!_.findWhere(value, { type: rateType._id })){
+////          result.push({ type: rateType._id, pay: pay*1.5, bill: bill*1.5 });
+////        }
+////        rateType = JobRateTypes.findOne({displayName:'Double Time'});
+////        if (!_.findWhere(value, { type: rateType._id })){
+////          result.push({ type: rateType._id, pay: pay*2, bill: bill*2 });
+////        }
+//        //</editor-fold>
+//
+//      }else if (obj['Direct Hire']){
+//        rateType = JobRateTypes.findOne({displayName:'Salary'});
+//        var pay= obj['Direct Hire'].salary;
+//        var bill= pay + pay*(obj['Direct Hire'].fee/100); //round 2 decimals
+//        bill= Math.round(bill * 100) / 100; //round 2 decimals
 //        if (!_.findWhere(value, { type: rateType._id })){
-//          result.push({ type: rateType._id, pay: pay*2, bill: bill*2 });
+//          result.push({ type: rateType._id, pay: pay, bill: bill });
 //        }
-        //</editor-fold>
-
-      }else if (obj['Direct Hire']){
-        rateType = JobRateTypes.findOne({displayName:'Salary'});
-        var pay= obj['Direct Hire'].salary;
-        var bill= pay + pay*(obj['Direct Hire'].fee/100); //round 2 decimals
-        bill= Math.round(bill * 100) / 100; //round 2 decimals
-        if (!_.findWhere(value, { type: rateType._id })){
-          result.push({ type: rateType._id, pay: pay, bill: bill });
-        }
-      }
+//      }
       return result;
     }
 })
