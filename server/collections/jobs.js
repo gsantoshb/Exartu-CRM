e84b95dd2f16b83af5c1c40431496868101eb7c9
psoto <psoto23.ps@gmail.com>
2014-09-05 14:52:43 -0300

    Copy job

--- server/collections/jobs.js
+++ server/collections/jobs.js
@@ -63,6 +63,18 @@ Meteor.startup(function () {
             }
           }
         )
+      },
+      'copyJob': function(jobId) {
+        var job = Jobs.findOne(jobId);
+        
+        if (job.hierId != Meteor.user().hierId)
+          throw new Meteor.Error(500, 'User not allowed to copy the job');
+
+        var jobCopy = _.pick(job, 'objNameArray', 'customer', 'hierId', 'jobTitle', 'duration', 'numberRequired', 'publicJobTitle');
+        // Default values
+        jobCopy.startDate = new Date();
+        
+        return Jobs.insert(jobCopy);
       }
     });
 });
