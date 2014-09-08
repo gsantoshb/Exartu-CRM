148d1b9266d37b4b0885ee6b4ff877c569ed59fc
javiber <javier.berneche@gmail.com>
2014-09-08 13:32:39 -0300

    make job tabs work like the ones in contactable using hash in the route

--- client/views/jobs/detail/job.js
+++ client/views/jobs/detail/job.js
@@ -1,19 +1,20 @@
 JobController = RouteController.extend({
-    layoutTemplate: 'mainLayout',
-    waitOn: function(){
-        return [JobHandler, ObjTypesHandler, GoogleMapsHandler]
-    },
-    data: function () {
-        Session.set('entityId', this.params._id);
-    },
-    action:function(){
-        if (!this.ready()) {
-            this.render('loadingContactable')
-            return;
-        }
-        this.render('job')
-    },
-  onAfterAction: function() {
+  layoutTemplate: 'mainLayout',
+  waitOn: function () {
+    return [JobHandler, ObjTypesHandler, GoogleMapsHandler]
+  },
+  data: function () {
+    Session.set('entityId', this.params._id);
+  },
+  action: function () {
+    if (!this.ready()) {
+      this.render('loadingContactable')
+      return;
+    }
+    Session.set('activeTab', this.params.hash);
+    this.render('job')
+  },
+  onAfterAction: function () {
     var title = 'Jobs / ' + Session.get('jobDisplayName'),
       description = 'Job information';
     SEO.set({
@@ -29,136 +30,136 @@ JobController = RouteController.extend({
   }
 });
 
-var generateReactiveObject = function(job) {
+var generateReactiveObject = function (job) {
   return new dType.objInstance(job, Jobs);
 };
 
-var self={};
+var self = {};
 Utils.reactiveProp(self, 'editMode', false);
-var location={};
+var location = {};
 Utils.reactiveProp(location, 'value', null);
 var services;
 
-Template.job.created=function(){
-  self.editMode=false;
-  var originalJob=Jobs.findOne({ _id: Session.get('entityId') });
+Template.job.created = function () {
+  self.editMode = false;
+  var originalJob = Jobs.findOne({ _id: Session.get('entityId') });
 
 
-  var definition={
-    reactiveProps:{
-      tags:{
+  var definition = {
+    reactiveProps: {
+      tags: {
         default: originalJob.tags,
         update: 'tags',
         type: Utils.ReactivePropertyTypes.array
       }
     }
   };
-  services= Utils.ObjectDefinition(definition);
+  services = Utils.ObjectDefinition(definition);
 }
 var job;
 Template.job.helpers({
-  job: function(){
-    var originalJob=Jobs.findOne({ _id: Session.get('entityId') });
+  job: function () {
+    var originalJob = Jobs.findOne({ _id: Session.get('entityId') });
     Session.set('jobDisplayName', originalJob.displayName);
     if (!job)
       job = generateReactiveObject(originalJob);
     return job;
   },
-  originalJob:function(){
+  originalJob: function () {
     return Jobs.findOne({ _id: Session.get('entityId') });
   },
-  editMode:function(){
-      return self.editMode;
+  editMode: function () {
+    return self.editMode;
   },
-  colorEdit:function(){
-      return self.editMode ? '#008DFC' : '#ddd'
+  colorEdit: function () {
+    return self.editMode ? '#008DFC' : '#ddd'
   },
-  isType:function(typeName){
-    return !! Jobs.findOne({ _id: Session.get('entityId'), objNameArray: typeName});
+  isType: function (typeName) {
+    return !!Jobs.findOne({ _id: Session.get('entityId'), objNameArray: typeName});
   },
-  jobCollection: function(){
+  jobCollection: function () {
     return Jobs;
   },
-  getCustomer:function(){
+  getCustomer: function () {
 
-    var j=Jobs.findOne({ _id: Session.get('entityId')});
+    var j = Jobs.findOne({ _id: Session.get('entityId')});
     return j && j.customer;
   },
-  noteCount: function() {
-      return Notes.find({links: { $elemMatch: { id: Session.get('entityId') } }}).count();
+  noteCount: function () {
+    return Notes.find({links: { $elemMatch: { id: Session.get('entityId') } }}).count();
   },
-  isSelected:function(optionValue, currentValue){
-      return optionValue == currentValue;
-    },
-  location: function(){
-    var originalJob=Jobs.findOne({ _id: Session.get('entityId') });
+  isSelected: function (optionValue, currentValue) {
+    return optionValue == currentValue;
+  },
+  location: function () {
+    var originalJob = Jobs.findOne({ _id: Session.get('entityId') });
 
-    location.value= originalJob && originalJob.location;
+    location.value = originalJob && originalJob.location;
     return location;
   },
-  tags: function(){
+  tags: function () {
     return services.tags;
   }
 });
 
 Template.job.events({
-  'click .editJob':function(){
-      self.editMode= ! self.editMode;
+  'click .editJob': function () {
+    self.editMode = !self.editMode;
   },
-  'click .saveButton':function(){
+  'click .saveButton': function () {
     if (!job.validate()) {
       job.showErrors();
       return;
     }
-    var update=job.getUpdate();
-    var originalJob=Jobs.findOne({ _id: Session.get('entityId') });
-    var oldLocation= originalJob.location;
-    var newLocation= location.value;
+    var update = job.getUpdate();
+    var originalJob = Jobs.findOne({ _id: Session.get('entityId') });
+    var oldLocation = originalJob.location;
+    var newLocation = location.value;
 
-    if ((newLocation && newLocation.displayName) != (oldLocation && oldLocation.displayName)){
-      update.$set= update.$set || {};
-      update.$set.location= newLocation;
+    if ((newLocation && newLocation.displayName) != (oldLocation && oldLocation.displayName)) {
+      update.$set = update.$set || {};
+      update.$set.location = newLocation;
     }
 
     if (services.tags.value.length > 0)
       update.$set.tags = services.tags.value;
 
-    Jobs.update({_id: job._id}, update, function(err, result) {
+    Jobs.update({_id: job._id}, update, function (err, result) {
       if (!err) {
-        self.editMode=false;
+        self.editMode = false;
         job.reset();
       }
     });
   },
-  'click .cancelButton':function(){
-      self.editMode=false;
+  'click .cancelButton': function () {
+    self.editMode = false;
   },
-  'click .add-tag': function() {
+  'click .add-tag': function () {
     addTag();
   },
-  'keypress #new-tag': function(e) {
+  'keypress #new-tag': function (e) {
     if (e.keyCode == 13) {
       e.preventDefault();
       addTag();
     }
   },
-  'click .remove-tag': function() {
+  'click .remove-tag': function () {
     services.tags.remove(this.value);
   },
-  'click #copy-job': function() {
+  'click #copy-job': function () {
     var result = confirm("Are you sure you want to copy this job?");
     if (result)
-      Meteor.call('copyJob', Session.get('entityId'), function(err, result) {
+      Meteor.call('copyJob', Session.get('entityId'), function (err, result) {
         if (!err) {
-          Router.go('/job/' +  result);
+          Router.go('/job/' + result);
         } else {
           console.log(err);
         }
-      });  
+      });
   }
 });
 
-var addTag = function() {
+var addTag = function () {
   var inputTag = $('#new-tag')[0];
 
   if (!inputTag.value)
@@ -172,7 +173,7 @@ var addTag = function() {
 };
 
 Template.job.helpers({
-  getType: function(){
+  getType: function () {
     return Enums.linkTypes.job;
   }
 })
@@ -180,37 +181,37 @@ Template.job.helpers({
 // Job description
 var jobDescriptionEditMode = false;
 var jobDescriptionEditModeDep = new Deps.Dependency;
-var descriptionSelf={}
+var descriptionSelf = {}
 Utils.reactiveProp(descriptionSelf, 'previewMode', false);
 
-Template.jobDescription.colorPreviewMode= function(){
+Template.jobDescription.colorPreviewMode = function () {
   return descriptionSelf.previewMode ? '#008DFC' : '#ddd'
 }
 
-Template.jobDescription.previewMode= function(){
+Template.jobDescription.previewMode = function () {
   return descriptionSelf.previewMode;
 }
 
-Template.jobDescription.editMode = function() {
+Template.jobDescription.editMode = function () {
   jobDescriptionEditModeDep.depend();
   return jobDescriptionEditMode;
 };
 
-Template.jobDescription.colorDescriptionEdit = function() {
+Template.jobDescription.colorDescriptionEdit = function () {
   jobDescriptionEditModeDep.depend();
   return jobDescriptionEditMode ? '#008DFC' : '#ddd';
 };
 
 Template.jobDescription.events = {
-  'click .editJobDescription': function(){
+  'click .editJobDescription': function () {
     jobDescriptionEditMode = !jobDescriptionEditMode;
     jobDescriptionEditModeDep.changed();
   },
-  'click #cancelJobDescriptionEdit':function(){
+  'click #cancelJobDescriptionEdit': function () {
     jobDescriptionEditMode = false;
     jobDescriptionEditModeDep.changed();
   },
-  'click #saveJobDescriptionEdit':function() {
+  'click #saveJobDescriptionEdit': function () {
     var update = job.getUpdate();
     if (!update.$set || !update.$set.jobDescription) {
       jobDescriptionEditMode = false;
@@ -218,7 +219,7 @@ Template.jobDescription.events = {
       return; // Nothing to update
     }
 
-    Jobs.update({_id: job._id}, {$set: { jobDescription: update.$set.jobDescription }}, function(err, result) {
+    Jobs.update({_id: job._id}, {$set: { jobDescription: update.$set.jobDescription }}, function (err, result) {
       if (!err) {
         jobDescriptionEditMode = false;
         jobDescriptionEditModeDep.changed();
@@ -226,7 +227,12 @@ Template.jobDescription.events = {
       }
     });
   },
-  'click .previewMode': function(){
-    descriptionSelf.previewMode= ! descriptionSelf.previewMode;
+  'click .previewMode': function () {
+    descriptionSelf.previewMode = !descriptionSelf.previewMode;
   }
 };
+
+Template.job_tabs.isActive = function(name){
+  var activeTab = Session.get('activeTab') || 'details';
+  return (name == activeTab) ? 'active' : '';
+}
