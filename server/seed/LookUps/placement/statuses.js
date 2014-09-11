a0d67f155e146d24f29d54f5fe0199da2378bee4
Gregg Dourgarian <greggd@tempworks.com>
2014-09-10 20:28:57 -0500

    limit placementStatus to Active/Inactive
    previously we used placement status also to distinguish direct-hire from contract but this was unnecessary because candidateStatus can do the job for us
    
    So candidate status supports the notion of 'placed' as an action...so if the user wants they can have a candidateStatus of 'Placed-contract' and 'Paced-direct-hire' if they want.

--- server/seed/LookUps/placement/statuses.js
+++ server/seed/LookUps/placement/statuses.js
@@ -1,13 +1,10 @@
 _.forEach([
   {
-    displayName: 'Assigned', lookUpActions:[Enums.lookUpAction.Implies_Active,Enums.lookUpAction.Placement_Assigned]
+    displayName: 'Active', lookUpActions:[Enums.lookUpAction.Implies_Active]
   },
   {
-    displayName: 'Candidate', lookUpActions:[Enums.lookUpAction.Implies_Active,Enums.lookUpAction.Placement_Candidate],isDefault: true
-  },
-    {
-      displayName: 'Inactive', lookUpActions:[Enums.lookUpAction.Implies_Inactive]
-    }
+    displayName: 'Inactive', lookUpActions:[Enums.lookUpAction.Implies_Inactive]
+  }
 ],
   function (item) {
     item.lookUpCode = Enums.lookUpTypes.placement.status.lookUpCode;
