30134addceaad9bf5377494d1013f074cc56f409
psoto <psoto23.ps@gmail.com>
2014-10-13 14:35:48 -0200

    Fix add job

--- client/views/jobs/add/addJob.js
+++ client/views/jobs/add/addJob.js
@@ -1,15 +1,19 @@
 JobAddController = RouteController.extend({
-    data: function(){
-        Session.set('objType',this.params.objType);
-    },
-    action: function () {
-        if (!this.ready()) {
-            this.render('loadingContactable');
-            return;
-        }
-        this.render('addJobPage');
-    },
-  onAfterAction: function() {
+  data: function () {
+    Session.set('objType', this.params.objType);
+  },
+  waitOn: function () {
+    return Meteor.subscribe('singleContactable', {houseAccount: true});
+  },
+  action: function () {
+    if (!this.ready()) {
+      this.render('loadingContactable');
+      return;
+    }
+
+    this.render('addJobPage');
+  },
+  onAfterAction: function () {
     var title = 'Add ' + Session.get('objType'),
       description = '';
     SEO.set({
@@ -26,66 +30,66 @@ JobAddController = RouteController.extend({
 });
 
 var model;
-var subTypesDep=new Deps.Dependency;
-var createJob= function(objTypeName){
-    var options= Session.get('addOptions');
-    if (options){
-      Session.set('addOptions', undefined);
-    }
+var subTypesDep = new Deps.Dependency;
+var createJob = function (objTypeName) {
+  var options = Session.get('addOptions');
+  if (options) {
+    Session.set('addOptions', undefined);
+  }
 
-    model= new dType.objTypeInstance(Session.get('objType'), options);
-    var defaultStatus=LookUps.findOne({lookUpCode: Enums.lookUpTypes.job.status.lookUpCode,isDefault:true});
-    if (defaultStatus) model.status=defaultStatus._id;
-    return model
-}
+  model = new dType.objTypeInstance(Session.get('objType'), options);
+  var defaultStatus = LookUps.findOne({lookUpCode: Enums.lookUpTypes.job.status.lookUpCode, isDefault: true});
+  if (defaultStatus) model.status = defaultStatus._id;
+  return model
+};
 
 Template.addJobPage.helpers({
-    model: function(){
-        if (!model){
-            model=createJob(Session.get('objType'));
-        }
-        return model;
-    },
-    subTypeArray: function(){
-        subTypesDep.depend();
-        return model.subTypes;
-    },
-    objTypeName: function(){
-        return Session.get('objType');
+  model: function () {
+    if (!model) {
+      model = createJob(Session.get('objType'));
     }
-})
+    return model;
+  },
+  subTypeArray: function () {
+    subTypesDep.depend();
+    return model.subTypes;
+  },
+  objTypeName: function () {
+    return Session.get('objType');
+  }
+});
 
 Template.addJobPage.events({
-    'click .btn-success': function(){
-        if (!dType.isValid(model)){
-            dType.displayAllMessages(model);
-            return;
-        }
-        var obj=dType.buildAddModel(model);
-        
-        // // hack to keep both titles the same
-        if (obj.jobTitle != null) {
-            obj.publicJobTitle = LookUps.findOne(obj.jobTitle).displayName;
-        }
-
-        Meteor.call('addJob', obj, function(err, result){
-            if(err){
-                console.dir(err)
-            }
-            else{
-              Meteor.call('setLastCustomerUsed', obj.customer, function(){
-                if(err)
-                  console.dir(err);
-              });
-              Router.go('/job/' + result._id);
-            }
-        });
-    },
-    'click .goBack': function(){
-        history.back();
+  'click .btn-success': function () {
+    if (!dType.isValid(model)) {
+      dType.displayAllMessages(model);
+      return;
     }
-})
+    var obj = dType.buildAddModel(model);
 
-Template.addJobPage.destroyed=function(){
-    model=undefined;
-}
+    // // hack to keep both titles the same
+    if (obj.jobTitle != null) {
+      obj.publicJobTitle = LookUps.findOne(obj.jobTitle).displayName;
+    }
+
+    Meteor.call('addJob', obj, function (err, result) {
+      if (err) {
+        console.dir(err)
+      }
+      else {
+        Meteor.call('setLastCustomerUsed', obj.customer, function () {
+          if (err)
+            console.dir(err);
+        });
+        Router.go('/job/' + result._id);
+      }
+    });
+  },
+  'click .goBack': function () {
+    history.back();
+  }
+});
+
+Template.addJobPage.destroyed = function () {
+  model = undefined;
+};
