09f0347ee2db03411d180a4fbc726c98747947fd
Gregg Dourgarian <greggd@tempworks.com>
2014-10-21 09:50:56 -0500

    checking in my code where i try to add the user name to the dashboard display...it will not bind {{userName}} properly

--- client/lib/_subscriptions/activities.js
+++ client/lib/_subscriptions/activities.js
@@ -1,2 +1,13 @@
-Activities = new Meteor.Collection("activitiesList");
+Activities = new Meteor.Collection("activitiesList" , {
+  transform: function (act) {
+    var u =     Meteor.users.findOne({_id : act.userId});
+
+    act.userName = "";
+    if (u)
+    {
+      if (u.emails && u.emails.length>0) act.userName=u.emails[0].address;
+    }
+    return act;
+  }
+});
 ActivitiesHandler = Meteor.paginatedSubscribe('activitiesList');
