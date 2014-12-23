03ed065550ea19891f08e526119ade0e1e5fe531
greggdourgarian <greggd@tempworks.com>
2014-12-22 21:29:03 -0600

    add lookup action 'Deleted' ... see explanation on Trello
    Let's user show that record could/should be deleted as it was mistakenly entered or somehow invalid.  By default a 'deleted' record is also inactive.

--- server/seed/LookUps/placement/statuses.js
+++ server/seed/LookUps/placement/statuses.js
@@ -6,7 +6,7 @@ _.forEach([
     displayName: 'Inactive', lookUpActions:[Enums.lookUpAction.Implies_Inactive]
   },
   {
-    displayName: "Deleted",lookUpActions: [Enums.lookUpAction.Implies_Inactive]
+    displayName: 'Deleted',lookUpActions:[Enums.lookUpAction.Implies_Inactive,Enums.lookUpAction.Implies_Deleted]
   }
 ],
   function (item) {
