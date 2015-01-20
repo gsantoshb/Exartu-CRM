33c21fed46c0787245755453d317db01e0cfbd4c
greggdourgarian <greggd@tempworks.com>
2015-01-19 21:38:00 -0600

    another person/organization fix to deal with bad data coming in from the api made decision to assume if an employee or contact that it is a person and to setup person fields accounting for possible null and zls values, same for customer

--- server/api/v1/contactables.js
+++ server/api/v1/contactables.js
@@ -108,47 +108,44 @@ var mapper = {
 		var contactable = {
 			objNameArray: ['contactable', type]
 		};
+		//
+		// api might be sending in faulty data such as a person with a blank or null last name so determine here
+		// whether it's a person or an organization based on its contactable type
+		// if it's a customer, assume organization
+		// set any blank fields for its type to 'NA'
+		if (type=='Employee' || type=='Contact') {
+			var personFields = ['firstName', 'lastName', 'middleName', 'jobTitle'];
+			_.some(personFields, function (fieldName) {
+				if (data[fieldName] == null || data[fieldName] == '') data[fieldName] = 'NA';
+			});
 
-		var personFields = ['firstName', 'lastName', 'middleName', 'jobTitle'];
-
-		// Check if it's a person
-		// might be sending over a person with a blank or null last and first name so check if those fields exist
-		// and if so set them to NA (not available)
-		var isPerson = _.some(personFields, function (fieldName) {
-			if (data[fieldName]==null || data[fieldName]=='') data[fieldName]='NA';
-			return !! data[fieldName]  ;
-		});
-
-		if (isPerson)
 			contactable.objNameArray.push('person');
 
-		_.forEach(personFields, function(personField) {
-			if (data[personField]) {
-				contactable.person = contactable.person || {};
-				contactable.person[personField] = data[personField];
-			}
-		});
+			_.forEach(personFields, function (personField) {
+				if (data[personField]) {
+					contactable.person = contactable.person || {};
+					contactable.person[personField] = data[personField];
+				}
+			});
+		}
+		else
+		{
 
-		var organizationFields = ['organizationName', 'department'];
-
-		// Check if it's an organization
-		var isOrganization = _.some(organizationFields, function (fieldName){
-			return !! data[fieldName];
-		});
-
-		if (isOrganization)
+			var organizationFields = ['organizationName', 'department'];
+			_.some(organizationFields, function (fieldName) {
+				if (data[fieldName] == null || data[fieldName] == '') data[fieldName] = 'NA';
+			});
 			contactable.objNameArray.push('organization');
 
-		_.forEach(organizationFields, function(organizationField) {
-			if (data[organizationField]) {
-				contactable.organization = contactable.organization || {};
-				contactable.organization[organizationField] = data[organizationField];
-			}
-		});
+			_.forEach(organizationFields, function (organizationField) {
+				if (data[organizationField]) {
+					contactable.organization = contactable.organization || {};
+					contactable.organization[organizationField] = data[organizationField];
+				}
+			});
+		}
 
-		contactable[type] = {
-			statusNote: data.status || ''
-		};
+		contactable[type] = {statusNote: data.status || ''};
 
     //if(data.email){
     //  var emailTypeId = ContactMethods.findOne({
