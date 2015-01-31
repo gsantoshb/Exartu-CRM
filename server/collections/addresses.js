ef024c35d63ad7c56160157a0f2797039d0df1be
greggdourgarian <greggd@tempworks.com>
2015-01-31 10:27:53 -0600

    add hierId (along w dateCreated and userId) to Addresses

--- server/collections/addresses.js
+++ server/collections/addresses.js
@@ -16,6 +16,8 @@ Addresses.allow({
 });
 
 Addresses.before.insert(function (userId, doc) {
+    var user=Meteor.users.find({_id:userId});
     doc.dateCreated=Date.now();
     doc.userId=userId;
+    doc.hierId=user.hierId;
 });
