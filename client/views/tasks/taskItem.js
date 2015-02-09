99cb6445180aae10234ec5a37a12429a6dbbb2fc
Ionut Titei <office@ionut-titei.ro>
2015-02-09 02:14:56 +0200

    some more visual updates

--- client/views/tasks/taskItem.js
+++ client/views/tasks/taskItem.js
@@ -1,51 +1,53 @@
 Template.taskItem.helpers({
-  taskClass: function(){
-    return 'task-' + this.state + (this.inactive ? ' inactive' : '');
-  },
-  isMe: function(){
-    return (Meteor.userId() == this._id) ? 'text-info' : '';
-  },
-  statusIs: function(statusName){
-    return this.state == Enums.taskState[statusName];
-  }
-})
-Template.taskItem.events({
-  'click .editTask': function () {
-    Utils.showModal('addEditTask', this)
-  }
-});
-Template.taskLabel.helpers({
-  statusIs: function(statusName){
-    return this.state == Enums.taskState[statusName];
-  },
-  getStatus: function(){
-    var keys = Object.keys(Enums.taskState);
-    var state = this.state;
-
-    for (var i = 0; i < keys.length; i++) {
-      if(Enums.taskState[keys[i]] == state)
-        return keys[i];
+    taskClass: function(){
+        return 'task-' + this.state + (this.inactive ? ' inactive' : '');
+    },
+    isMe: function(){
+        return (Meteor.userId() == this._id) ? 'text-info' : '';
+    },
+    statusIs: function(statusName){
+        return this.state == Enums.taskState[statusName];
+    },
+    hasLinks: function() {
+        return this.links.length;
+    },
+    getHref: function(){
+        return Utils.getHrefFromLink(this);
+    },
+    getEntity: function(){
+        return Utils.getEntityFromLink(this);
     }
-  },
-  hasLinks: function() {
-    return this.links.length;
-  },
-  canComplete: function(){
-    return this.state == Enums.taskState.pending && _.contains(this.assign,Meteor.userId());
-  },
-  getStateIcon: function(){
-    return helper.getTaskStateIcon(this.state);
-  },
-  getHref: function(){
-    return Utils.getHrefFromLink(this);
-  },
-  getEntity: function(){
-    return Utils.getEntityFromLink(this);
-  }
-})
+});
+
+Template.taskItem.events({
+    'click .editTask': function (){
+        Utils.showModal('addEditTask', this)
+    }
+});
+
+Template.taskLabel.helpers({
+    statusIs: function(statusName){
+        return this.state == Enums.taskState[statusName];
+    },
+    getStatus: function(){
+        var keys = Object.keys(Enums.taskState);
+        var state = this.state;
+
+        for (var i = 0; i < keys.length; i++) {
+            if(Enums.taskState[keys[i]] == state)
+                return keys[i];
+        }
+    },
+    canComplete: function(){
+        return this.state == Enums.taskState.pending && _.contains(this.assign,Meteor.userId());
+    },
+    getStateIcon: function(){
+        return helper.getTaskStateIcon(this.state);
+    }
+});
 
 Template.showTaskDate.helpers({
-  statusIs: function(statusName){
-    return this.state == Enums.taskState[statusName];
-  }
-})
+    statusIs: function(statusName){
+        return this.state == Enums.taskState[statusName];
+    }
+});
