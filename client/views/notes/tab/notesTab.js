9567628012cded5fe65f081791b4dbc19d7b845d
greggdourgarian <greggd@tempworks.com>
2015-02-09 17:10:40 -0600

    use confirm instead of modal to confirm sms to hotlist...modal does not wait...

--- client/views/notes/tab/notesTab.js
+++ client/views/notes/tab/notesTab.js
@@ -49,28 +49,20 @@ AutoForm.hooks({
     AddNoteRecord: {
         before: {
             addContactableNote: function (doc) {
-                if (doc.sendAsSMS && Session.get('entityId')==Session.get('hotListId'))
-                {
-                    var hotlist=HotLists.findOne(Session.get('hotListId'));
-                    if (!hotlist || !hotlist.members) return false;
-                    Utils.showModal('basicModal', {
-                        title: 'Confirm send' ,
-                        message: 'Send to the ' + hotlist.members.length + ' members of hot list \'' + hotlist.displayName + '\'.  Continue?',
-                        buttons: [{label: 'Cancel', classes: 'btn-default', value: false}, {label: 'Send', classes: 'btn-success', value: true}],
-                        callback: function (result) {
-                            if (!result) {
-                                return false;
-                            }
-                        }
-                    });
-
-                };
                 var initialLink = {
                     id: Session.get('entityId'),
                     type: Utils.getEntityTypeFromRouter()
                 };
                 doc.links = doc.links || [initialLink];
                 doc.contactableId = Session.get('entityId');
+                if (doc.sendAsSMS && Session.get('entityId')==Session.get('hotListId'))
+                {
+                    var hotlist=HotLists.findOne(Session.get('hotListId'));
+                    if (!hotlist || !hotlist.members) return false;
+                    var message= 'Send to the ' + hotlist.members.length + ' members of hot list \'' + hotlist.displayName + '\'.  Continue?';
+                    return  (confirm (message)) ? doc: false;
+
+                };
                 return doc;
             }
         }
