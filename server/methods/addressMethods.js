6a41a470fbf40280e2806f787a0a9812640dd4ff
greggdourgarian <greggd@tempworks.com>
2015-02-11 19:36:48 -0600

    api correction for address insert

--- server/methods/addressMethods.js
+++ server/methods/addressMethods.js
@@ -2,13 +2,13 @@
  * Created by visualaram on 1/27/15.
  */
 Meteor.methods({
-  addEditAddress: function (addr) {
-    return AddressManager.addEditAddress(addr);
-  },
-  removeAddress: function(id) {
-    AddressManager.removeAddress(id);
-  },
-    getAddress: function (contactableid,addresstype) {
-        return AddressManager.getAddress(contactableid,addresstype);
+    addEditAddress: function (addr) {
+        return AddressManager.addEditAddress(addr);
+    },
+    removeAddress: function (id) {
+        AddressManager.removeAddress(id);
+    },
+    getAddress: function (contactableid, addresstype) {
+        return AddressManager.getAddress(contactableid, addresstype);
     }
 });
