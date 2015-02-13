fdb611f38fa02e4a54c2a524cd14fee6a64467dd
greggdourgarian <greggd@tempworks.com>
2015-02-12 22:41:26 -0600

    move the list of admin email addresses (greggd@tempworks.com etc) to ExartuConfig

--- server/managers/roleManager.js
+++ server/managers/roleManager.js
@@ -36,17 +36,7 @@ RoleManager = {
   bUserIsSystemAdmin: function (user)
   {
     if (!user) return false;
-    if (user && user.emails[0] && user.emails[0].address.toLowerCase() == 'greggd@aidacreative.com') return true;
-    if (user && user.emails[0] && user.emails[0].address.toLowerCase() == 'greggd@tempworks.com') return true;
-    if (user && user.emails[0] && user.emails[0].address.toLowerCase() == 'gregg1@tempworks.com') return true;
-    if (user && user.emails[0] && user.emails[0].address.toLowerCase() == 'aram.gugusian@aidacreative.com') return true;
-    if (user && user.emails[0] && user.emails[0].address.toLowerCase() == 'nelson.campos@aidacreative.com') return true;
-    if (user && user.emails[0] && user.emails[0].address.toLowerCase() == 'alex.armstrong@aidacreative.com') return true;
-    if (user && user.emails[0] && user.emails[0].address.toLowerCase() == 'robert.armstrong@aidacreative.com') return true;
-    if (user && user.emails[0] && user.emails[0].address.toLowerCase() == 'jiawei.mo@aidacreative.com') return true;
-    if (user && user.emails[0] && user.emails[0].address.toLowerCase() == 'javier.berneche@aidacreative.com') return true;
-    if (user && user.emails[0] && user.emails[0].address.toLowerCase() == 'mihai.constantinescu@east-wolf.com') return true;
-    if (user && user.emails[0] && user.emails[0].address.toLowerCase() == 'ionut.titei@east-wolf.com') return true;
+    if (user && user.emails[0] && _.contains(ExartuConfig.AdminEmails,user.emails[0].address.toLowerCase())) return true;
     return RoleManager.bUserHasRoleId(user,this.getSystemAdministratorRole()._id)
   },
   bUserIsClientAdmin: function (user)
