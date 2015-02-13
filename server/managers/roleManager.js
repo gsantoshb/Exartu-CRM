43b6ea8b37fe615911de41ec2e40ac87d1d8ee66
greggdourgarian <greggd@tempworks.com>
2015-02-12 22:56:50 -0600

    hmmm AdminEmails config variable not working on Heroku.  Inserting console statements to see what's up.

--- server/managers/roleManager.js
+++ server/managers/roleManager.js
@@ -2,54 +2,53 @@
 //
 //
 RoleManager = {
-  getRoleFromName: function (name) {
-    return Roles.findOne({name: name});
-  },
-  getRoleFromId: function(id)
-  {
-    return Roles.findOne({_id:id});
-  },
-  getSystemAdministratorRole: function() {
-    return RoleManager.getRoleFromName(Enums.roleFunction.System_Administrator);
-  },
-  getClientAdministratorRole: function() {
-    return RoleManager.getRoleFromName(Enums.roleFunction.Client_Administrator);
-  },
-  getUserRoles: function(user) {
-    if (!user) return [];
-    if (!user.hierRoles) return [];
-    var hr =_.findWhere(user.hierRoles,{hierId: user.currentHierId});
-    var roles=(hr) ? hr.roleIds : [];
-    return roles.map(function(item){ return item.roleId})
-  },
-  bUserHasRoleId: function(user,id)
-  {
-    var roleIds=RoleManager.getUserRoles(user);
-    if (!_.contains(roleIds, id)) return false;
-    return true;
-  },
-  bUserHasRoleName: function(user,name)
-  {
-    if (!_.contains(RoleManager.getUserRoles(user), RoleManager.getRoleFromName(name)._id)) return false;
-    return true;
-  },
-  bUserIsSystemAdmin: function (user)
-  {
-    if (!user) return false;
-    if (user && user.emails[0] && _.contains(ExartuConfig.AdminEmails,user.emails[0].address.toLowerCase())) return true;
-    return RoleManager.bUserHasRoleId(user,this.getSystemAdministratorRole()._id)
-  },
-  bUserIsClientAdmin: function (user)
-  {
-    var role=this.getClientAdministratorRole();
-    if (role && role._id)
-    return RoleManager.bUserHasRoleId(user,role._id);
-    else
-      return false;
-  },
-    bUserIsAdmin: function(user)
-    {
-        return (RoleManager.bUserIsClientAdmin(user)|| RoleManager.bUserIsSystemAdmin(user));
+    getRoleFromName: function (name) {
+        return Roles.findOne({name: name});
+    },
+    getRoleFromId: function (id) {
+        return Roles.findOne({_id: id});
+    },
+    getSystemAdministratorRole: function () {
+        return RoleManager.getRoleFromName(Enums.roleFunction.System_Administrator);
+    },
+    getClientAdministratorRole: function () {
+        return RoleManager.getRoleFromName(Enums.roleFunction.Client_Administrator);
+    },
+    getUserRoles: function (user) {
+        if (!user) return [];
+        if (!user.hierRoles) return [];
+        var hr = _.findWhere(user.hierRoles, {hierId: user.currentHierId});
+        var roles = (hr) ? hr.roleIds : [];
+        return roles.map(function (item) {
+            return item.roleId
+        })
+    },
+    bUserHasRoleId: function (user, id) {
+        var roleIds = RoleManager.getUserRoles(user);
+        if (!_.contains(roleIds, id)) return false;
+        return true;
+    },
+    bUserHasRoleName: function (user, name) {
+        if (!_.contains(RoleManager.getUserRoles(user), RoleManager.getRoleFromName(name)._id)) return false;
+        return true;
+    },
+    bUserIsSystemAdmin: function (user) {
+        if (!user) return false;
+        console.log('AdminEmails:', ExartuConfig.AdminEmails);
+        console.log('users email', user.emails[0].address.toLowerCase());
+        console.log('contains', _.contains(ExartuConfig.AdminEmails, user.emails[0].address.toLowerCase()));
+        if (user && user.emails[0] && _.contains(ExartuConfig.AdminEmails, user.emails[0].address.toLowerCase())) return true;
+        return RoleManager.bUserHasRoleId(user, this.getSystemAdministratorRole()._id)
+    },
+    bUserIsClientAdmin: function (user) {
+        var role = this.getClientAdministratorRole();
+        if (role && role._id)
+            return RoleManager.bUserHasRoleId(user, role._id);
+        else
+            return false;
+    },
+    bUserIsAdmin: function (user) {
+        return (RoleManager.bUserIsClientAdmin(user) || RoleManager.bUserIsSystemAdmin(user));
     }
 
 };
