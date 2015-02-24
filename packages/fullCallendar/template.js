a8d606e78deace7efbec531ce0c93b97f89c3075
javiber <javier.berneche@gmail.com>
2015-02-24 13:27:36 -0200

    add fullcalendar package as local package the fullcalendar package has a dependency on jquery-ui, this brings conflicts with other packages such as the dateTimePicker.

---
+++ packages/fullCallendar/template.js
@@ -0,0 +1,7 @@
+Template.fullcalendar.rendered = function() {
+  var div = this.$(this.firstNode);
+  //jquery takes care of undefined values, no need to check here
+  div.attr('id', this.data.id);
+  div.addClass(this.data.class);
+  div.fullCalendar(this.data);
+}
