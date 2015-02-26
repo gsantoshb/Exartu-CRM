b75400d9cfbb733d1a9fc4f365fd2efae12a0e91
greggdourgarian <greggd@tempworks.com>
2015-02-26 15:56:23 -0600

    store computed description string fields on job (clientDisplayName, jobTitleDisplayName and displayName)) and placement (jobDisplayName, employeeDisplayName,displayName). This is so search text searches can go directly against data already in the document instead of having to improvise join type logic.

--- server/collections/jobs.js
+++ server/collections/jobs.js
@@ -110,11 +110,22 @@ Jobs.before.insert(function (userId, doc) {
     } catch (e) {
         var user = {}
     }
+
     doc.hierId = user.currentHierId || doc.hierId;
     doc.userId = user._id || doc.userId;
     doc.dateCreated = Date.now();
     if (!doc.activeStatus) doc.activeStatus = LookUpManager.getActiveStatusDefaultId();
+    if (doc.client) {
+        var c = Contactables.findOne(doc.client);
+        if (c) doc.clientDisplayName = c.displayName;
+    };
 
+    if (doc.jobTitle){
+        var jt = LookUps.findOne(doc.jobTitle);
+        if (jt) doc.jobTitleDisplayName=jt.displayName;
+    }
+    doc.displayName=doc.jobTitleDisplayName + ' @ ' + doc.clientDisplayName;
+    if (!doc.tags) doc.tags=[];
 });
 
 // Indexes
