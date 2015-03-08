4fd348fbdbfdfc690051d162f6e192f20e8884b1
greggdourgarian <greggd@tempworks.com>
2015-03-08 13:16:15 -0500

    the parse resume file process is sometimes double or triple processing the selected resume.  Put in code to say same file can not be reprocessed during a given browser session.

--- packages/subscription-plan/common.js
+++ packages/subscription-plan/common.js
@@ -12,7 +12,7 @@ SubscriptionPlan.getUserPlan = function() {
 //  console.dir(user);
 
   var hier = Hierarchies.findOne({_id: user.currentHierId});
-    console.log('user',user);
+    //console.log('user',user);
 
   if(!hier)
     throw new Meteor.Error(404, 'Hierarchy not found');
