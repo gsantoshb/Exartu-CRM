aff76d6fd7c600cb99d4a3f80e16fc49750cc94f
Ionut Titei <office@ionut-titei.ro>
2015-04-03 19:30:34 +0300

    contactable detail page spacing fix / popover issue fixed again

--- client/views/contactables/list/contactables.js
+++ client/views/contactables/list/contactables.js
@@ -385,24 +385,7 @@ Template.contactablesList.created = function () {
     });
 };
 
-Template.contactablesList.rendered = function () {
-    /**
-     * @todo review code, this ia a small hack to make ti work.
-     * This particular plugin doesn't seem to behave quite right if you initialize it more than once so we're doing it on each first click event.
-     */
-    //$(document).on('click', 'button[data-toggle="popover"]', function (e) {
-    //    var object = e.currentTarget;
-    //    // destroy any other popovers open on page
-    //    $('.popover').popover('destroy');
-    //
-    //    if ($(object).attr('data-init') == 'off') {
-    //        // we set all other popovers besides this one to off so that we can open them next time
-    //        $('button[data-toggle="popover"]').attr('data-init', 'off');
-    //        $(object).popover('show');
-    //        $(object).attr('data-init', 'on');
-    //    }
-    //});
-};
+Template.contactablesList.rendered = function () {};
 
 // hack: because the handler is created on the created hook, the SubscriptionHandlers 'cleaner' can't find it
 Template.contactablesList.destroyed = function () {
@@ -411,10 +394,12 @@ Template.contactablesList.destroyed = function () {
     //    delete SubscriptionHandlers.AuxContactablesHandler;
     //}
 
+    $('button[data-toggle="popover"]').attr('data-init', 'off');
     $('.popover').hide().popover('destroy');
 };
 
 Template.contactablesListItem.destroyed = function () {
+    $('button[data-toggle="popover"]').attr('data-init', 'off');
     $('.popover').hide().popover('destroy');
 };
 
@@ -718,15 +703,13 @@ Template.contactables.events({
     },
     'click button[data-toggle="popover"]': function (e, ctx) {
         var object = e.currentTarget;
+        var attr = $(object).attr('aria-describedby');
         // destroy any other popovers open on page
         $('.popover').popover('destroy');
 
-        if ($(object).attr('data-init') == 'off') {
+        if ( !(typeof attr !== typeof undefined && attr !== false) ) {
             // we set all other popovers besides this one to off so that we can open them next time
-            $('button[data-toggle="popover"]').attr('data-init', 'off');
             $(object).popover('show');
-            console.log('show');
-            $(object).attr('data-init', 'on');
         }
     }
 });
