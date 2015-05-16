4d2e958d2d2cfb39aaaf16c33c4d014b05eecf9c
n-campos <ncampos@tempworks.com>
2015-05-16 20:07:07 -0300

    improved migration 26

--- server/migrations/26_latestNotes.js
+++ server/migrations/26_latestNotes.js
@@ -1,25 +1,31 @@
+
 Migrations.add({
   version: 26,
   up: function () {
-    //clear all tags
-    var contactables = Contactables.find({}).fetch();
-    _.each(contactables, function(c){
-       var newLatestNote = [];
-       var n = Notes.find({'links.id':c._id},{$sort:{dateCreated:-1} }).fetch();
-       if(n) {
-         if (n[2]) {
+    var count = 0;
+
+    Contactables.find({latestNotes: {$exists: false}}, {fields: {_id: 1}}).forEach(function (c) {
+      count++;
+      console.log('updating contactable', count, '-', c._id);
+
+      var newLatestNote = [];
+      var n = Notes.find({'links.id': c._id}, {$sort: {dateCreated: -1}, limit:3}).fetch();
+      if (n) {
+        if (n[2]) {
           newLatestNote.push(n[2]);
-         }
-         if (n[1]) {
+        }
+        if (n[1]) {
           newLatestNote.push(n[1]);
-         }
-         if (n[0]) {
+        }
+        if (n[0]) {
           newLatestNote.push(n[0]);
-         }
-       }
-       Contactables.update({_id:c._id},{$set: {latestNotes: newLatestNote}, $unset:{lastNote:""}});
-    })
-    }
+        }
+      }
+
+      // Update contactable latest notes
+      Contactables.update({_id: c._id}, {$set: {latestNotes: newLatestNote}, $unset: {lastNote: ""}});
+    });
+
+    console.log('Finished migration 26');
+  }
 });
-
-
