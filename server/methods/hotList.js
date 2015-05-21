44ba61ece6f6745834c6bac43f2f38d7d645eb4a
javiber <javier.berneche@gmail.com>
2015-05-21 16:52:39 -0300

    use new ContactablesView

--- server/methods/hotList.js
+++ server/methods/hotList.js
@@ -13,27 +13,37 @@ Meteor.methods({
     }
   },
 
-    'hotListTextMessageSend': function (msg, id) {
-        return HotListManager.hotListTextMessageSend(msg,id);
-    },
-    'getValidResponseMembers': function(hotlist){
-       var hotlistMembers = hotlist.members;
-       var validMembers = [];
-       _.forEach(hotlistMembers, function(m){
-         var result = HotLists.findOne({_id:{$ne: hotlist._id},dateCreated:{$gte:  hotlist.dateCreated}, members:{$in: [m]}  })
-         if(!result){
-           validMembers.push(m);
-         }
-       })
-      return validMembers;
-
-
-    },
-    'updateHotList': function(hotlist){
-      try {
-        return HotListManager.updateHotList(hotlist);
-      } catch (err) {
-        throw new Meteor.Error(err.message);
-      }
+  'hotListTextMessageSend': function (msg, id) {
+      return HotListManager.hotListTextMessageSend(msg,id);
+  },
+  'getValidResponseMembers': function(hotlist){
+     var hotlistMembers = hotlist.members;
+     var validMembers = [];
+     _.forEach(hotlistMembers, function(m){
+       var result = HotLists.findOne({_id:{$ne: hotlist._id},dateCreated:{$gte:  hotlist.dateCreated}, members:{$in: [m]}  })
+       if(!result){
+         validMembers.push(m);
+       }
+     });
+    return validMembers;
+  },
+  'updateHotList': function(hotlist){
+    try {
+      return HotListManager.updateHotList(hotlist);
+    } catch (err) {
+      throw new Meteor.Error(err.message);
     }
+  },
+
+  'findHotList': function (categories, text) {
+    return Utils.filterCollectionByUserHier2(Meteor.userId(), HotLists.find({
+      category: {$in: categories},
+      displayName: {$regex: ".*" + text + ".*", $options: 'i'}
+    },{
+      displayName: 1
+    })).fetch();
+  },
+  'addMembersToHotList': function (hotListId, membersId) {
+    return HotListManager.addToHotlist(hotListId, membersId);
+  }
 });
