41853114eea86e1bb9092276a6e1231ab3e4807e
ramiro <lidnele@hotmail.com>
2015-06-23 15:57:29 -0300

    placement preview

--- client/lib/afCustomInputs/customInputs.js
+++ client/lib/afCustomInputs/customInputs.js
@@ -46,4 +46,71 @@ AutoForm.addInputType('afSimpleCheckbox', {
     delete context.atts.required;
     return context;
   }
-});
+})
+
+
+
+
+AutoForm.addInputType('afButtonGroup', {
+  template: 'afButtonGroup',
+  valueOut: function (ctx) {
+    return Blaze.getData(this[0]).selected.get();
+  },
+  contextAdjust: function(ctx){
+    ctx.buttonArray = ctx.atts.buttonArray;
+    delete ctx.atts.buttonArray;
+    ctx.selected = ctx.atts.selected;
+    delete ctx.atts.selected;
+    return ctx;
+  }
+})
+
+
+var actualizeButtons = function(self){
+  var newArray = [];
+  _.each(self.data.buttonArray.get(), function(b){
+    if(self.data.selected.get() === b.value){
+      _.extend(b,{clase:"btn btn-sm btn-primary"});
+      newArray.push(b)
+    }
+    else{
+      _.extend(b,{clase:"btn btn-sm btn-default"});
+      newArray.push(b)
+    }
+
+  })
+  self.data.buttonArray.set(newArray);
+}
+
+Template.afButtonGroup.created = function(){
+  var selected = new ReactiveVar();
+  var buttonArray = new ReactiveVar();
+  selected.set(this.data.selected);
+  buttonArray.set(this.data.buttonArray);
+  this.data.selected = selected;
+  this.data.buttonArray = buttonArray;
+  actualizeButtons(this);
+}
+
+Template.afButtonGroup.rendered = function(){
+  //actualizeButtons(this);
+}
+
+Template.afButtonGroup.events({
+  'click .buttonGroup': function(e, ctx){
+    if(ctx.data.selected.get()=== e.target.id){
+      ctx.data.selected.set(null);
+    }
+    else {
+      ctx.data.selected.set(e.target.id);
+    }
+    actualizeButtons(ctx);
+  }
+})
+
+Template.afButtonGroup.helpers({
+   buttonsGroup: function(){
+
+      return this.buttonArray.get();
+   }
+})
--- client/views/contactables/detail/details/detail-box.js
+++ client/views/contactables/detail/details/detail-box.js
@@ -1,3 +1,54 @@
+schemaEditContactable = new SimpleSchema({
+  'personFirstName': {
+    type: String,
+    optional: true
+  },
+  'personMiddleName': {
+    type: String,
+    optional: true
+  },
+  'personLastName': {
+    type: String,
+    optional: true
+  },
+  'personJobTitle': {
+    type: String,
+    optional: true
+  },
+  'personBirthDate':{
+    type: Date,
+    optional: true
+  },
+  'organizationOrganizationName':{
+    type: String,
+    optional: true
+  },
+  'clientDepartment':{
+    type:String,
+    optional:true
+  },
+  'clientStatus':{
+      type:String,
+      optional:true
+  },
+  'employeeStatus':{
+    type:String,
+    optional:true
+  },
+  'contactStatus':{
+    type:String,
+    optional:true
+  }
+
+  //phone: {
+  //  type: String,
+  //  regEx:/^[\+]?[\s\(\)\-\d]+[\s]*$/,
+  //  optional: true
+  //}
+});
+
+
+
 EditDirectDepositMode = {
   val: false,
   dep: new Tracker.Dependency,
@@ -55,7 +106,28 @@ Template.contactableDetailBox.onRendered(function () {
   userSelected.set(this.data.userId);
 });
 
+
 Template.contactableDetailBox.helpers({
+    buttonsActiveStatus: function(){
+      var arrayButtons = [{displayName:"Active", value:"Active"},{displayName:"Inactive",value:"Inactive"}];
+      return arrayButtons;
+    },
+    buttonsProcessStatus: function(){
+      var arrayButtons =[];
+      if(this.Client){
+        arrayButtons = [{displayName: "Lost", value: "Lost"}, {displayName: "Prospect", value: "Prospect"},
+          {displayName: "Need Analysis", value: "NeedAnalysis"}, {displayName: "Proposal", value: "Proposal"}, {
+            displayName: "Negotiation",value: "Negotiation"},{displayName: "Won",value: "Won"}];
+      }
+      else {
+        arrayButtons = [{displayName: "Lead", value: "Lead"}, {displayName: "Applicant", value: "Applicant"},
+          {displayName: "Candidate", value: "Candidate"}, {displayName: "Hired", value: "Hired"}, {
+            displayName: "Other",
+            value: "Other"
+          }];
+      }
+        return arrayButtons;
+    },
     created: function () {
         EditMode.hide();
     },
@@ -63,8 +135,9 @@ Template.contactableDetailBox.helpers({
         return value1 == value2
     },
     contactable: function () {
-        contactable = new dType.objInstance(this, Contactables);
-        return contactable;
+
+        //contactable = new dType.objInstance(this, Contactables);
+        return this;
     },
     originalContactable: function () {
         return Contactables.findOne(this._id);
@@ -79,22 +152,39 @@ Template.contactableDetailBox.helpers({
         return EditMode.value ? '#008DFC' : '';
     },
     fetchOptions: function () {
-        return Utils.sortFetchOptions(this.options);
+      return Utils.sortFetchOptions(this.options);
     },
-    lostClient: function () {
-        statusDep.depend();
-        if (contactable.status.value) {
-            var lkp = LookUps.findOne({_id: contactable.status.value});
-            if (lkp) return (_.contains(lkp.lookUpActions, Enums.lookUpAction.Client_Lost));
-        }
-        return false;
-    },
+    //lostClient: function () {
+    //    statusDep.depend();
+    //    if (this.Client && this.Client.status) {
+    //        var lkp = LookUps.findOne({_id: this.Client.status});
+    //        if (lkp) return (_.contains(lkp.lookUpActions, Enums.lookUpAction.Client_Lost));
+    //    }
+    //    return false;
+    //},
     onSelectedStatus: function () {
         return function (newStatus) {
             var ctx = Template.parentData(2);
             ctx.property._value = newStatus;
             statusDep.changed();
         }
+    },
+    getActiveStatus: function(){
+      var activeStatus = LookUps.findOne({_id:this.activeStatus});
+      return activeStatus.displayName;
+    },
+    getProcessStatus: function(){
+      var processStatus;
+      if(this.Employee) {
+        processStatus = LookUps.findOne({_id: this.Employee.status});
+      }
+      else if(this.Contact){
+        processStatus = LookUps.findOne({_id: this.Contact.status});
+      }
+      else if(this.Client){
+        processStatus = LookUps.findOne({_id: this.Client.status});
+      }
+        return processStatus.displayName;
     },
     hideTaxID: function () {
         return hideTaxID.get();
