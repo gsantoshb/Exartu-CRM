77121bf8ed11be422f3e2fd21c67926bc60b09da
n-campos <ncampos@tempworks.com>
2015-06-26 16:39:18 -0300

    reorganized tasks collection hooks

--- server/collections/activities.js
+++ server/collections/activities.js
@@ -424,25 +424,6 @@ Meteor.paginatedPublish(ActivityViews, function (id) {
 });
 
 
-// Tasks
-Tasks.after.insert(function (userId, doc) {
-  var obj = {
-    userId: doc.userId,
-    hierId: doc.hierId,
-    type: Enums.activitiesType.taskAdd,
-    entityId: doc._id,
-    links: _.map(doc.links, function (link) {
-      return link.id;
-    }),
-    data: {
-      taskId: doc._id,
-      dateCreated: new Date()
-    }
-  };
-  if (doc && doc.testData) obj.testData = true;
-  Activities.insert(obj);
-});
-
 //Placements
 Placements.after.insert(function (userId, doc) {
   var data = {};
--- server/collections/tasks.js
+++ server/collections/tasks.js
@@ -125,16 +125,8 @@ Tasks.allow({
     }
 });
 
-Tasks.before.insert(function (userId, doc) {
-    var user = Meteor.user();
-    doc.hierId = user.currentHierId;
-    doc.userId = user._id;
-    doc.dateCreated = doc.dateCreated || Date.now();
-    doc.task = true
-});
 
 // Indexes
-
 Tasks._ensureIndex({hierId: 1});
 Tasks._ensureIndex({assign: 1});
 Tasks._ensureIndex({userId: 1});
