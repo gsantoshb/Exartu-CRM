9385a677c7bfa7600bccf94843099d7eccccd325
javiber <javier.berneche@gmail.com>
2015-07-06 17:39:50 -0300

    fix createdby

--- server/collections/hooks/contactables/beforeInsert.js
+++ server/collections/hooks/contactables/beforeInsert.js
@@ -14,7 +14,7 @@ Contactables.before.insert(function (userId, doc) {
 
   doc.hierId = user.currentHierId || doc.hierId;
   doc.userId = user._id || doc.userId;
-  doc.createdBy = user.userId;
+  doc.createdBy = doc.userId;
   doc.dateCreated = Date.now();
   if (!doc.activeStatus) doc.activeStatus = LookUpManager.getActiveStatusDefaultId();
   if (doc.organization) {
