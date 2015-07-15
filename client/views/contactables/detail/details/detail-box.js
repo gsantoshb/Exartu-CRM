9763e011c8686ad7fd23559c8d4b2e6dcafd8e62
ramiro <lidnele@hotmail.com>
2015-07-15 18:11:47 -0300

    separate biographical detail from work interes on contactable detail

--- client/views/contactables/detail/details/detail-box.js
+++ client/views/contactables/detail/details/detail-box.js
@@ -1,42 +1,4 @@
-AddressSchema =new SimpleSchema({
-  address: {
-    type: String,
-    optional:true
-  },
-  lat: {
-    type: Number,
-    decimal: true,
-    optional:true
-  },
-  lng: {
-    type: Number,
-    decimal: true,
-    optional:true
-  },
-  street: {
-    type: String,
-    max: 100,
-    optional:true
-  },
-  city: {
-    type: String,
-    max: 50,
-    optional:true
-  },
-  state: {
-    type: String,
-    optional:true
-  },
-  zip: {
-    type: String,
-    regEx: /^[0-9]{5}$/,
-    optional:true
-  },
-  country: {
-    type: String,
-    optional:true
-  }
-});
+
 
 
 
@@ -338,49 +300,6 @@ Template.contactableDetailBox.helpers({
           toReturn.Employee = true;
           toReturn.taxID = this.Employee.taxID;
           toReturn.employeeStatus = this.Employee.status;
-          toReturn.hasTransportation = this.Employee.hasTransportation;
-          toReturn.desiredPay = this.Employee.desiredPay;
-          toReturn.dateAvailable = this.Employee.dateAvailable;
-          if(this.Employee.availableStartDate){
-            var avaiableStart = [];
-            if(this.Employee.availableStartDate['0'] === true){
-              avaiableStart.push("Sunday");
-            } if(this.Employee.availableStartDate['1'] === true){
-              avaiableStart.push("Monday");
-            } if(this.Employee.availableStartDate['2'] === true){
-              avaiableStart.push("Tuesday");
-            } if(this.Employee.availableStartDate['3'] === true){
-              avaiableStart.push("Wednesday");
-            } if(this.Employee.availableStartDate['4'] === true){
-              avaiableStart.push("Thursday");
-            } if(this.Employee.availableStartDate['5'] === true){
-              avaiableStart.push("Friday");
-            } if(this.Employee.availableStartDate['6'] === true){
-              avaiableStart.push("Saturday");
-            }
-            toReturn.availableStartDate = avaiableStart;
-          }
-          if(this.Employee.availableShifts){
-            var avaiableSh = [];
-            if(this.Employee.availableShifts['1'] === true){
-              avaiableSh.push("1st shift");
-            } if(this.Employee.availableShifts['2'] === true){
-              avaiableSh.push("2nd shift");
-            } if(this.Employee.availableShifts['3'] === true){
-              avaiableSh.push("3rd shift");
-            }
-            toReturn.availableShifts = avaiableSh;
-          }
-          if(this.Employee.preferredWorkLocation) {
-            toReturn.preferredWorkLocation = {};
-            toReturn.preferredWorkLocation.street = this.Employee.preferredWorkLocation.address;
-            toReturn.preferredWorkLocation.state = this.Employee.preferredWorkLocation.state;
-            toReturn.preferredWorkLocation.city = this.Employee.preferredWorkLocation.city;
-            toReturn.preferredWorkLocation.country = this.Employee.preferredWorkLocation.country;
-            toReturn.preferredWorkLocation.lat = this.Employee.preferredWorkLocation.lat;
-            toReturn.preferredWorkLocation.lng = this.Employee.preferredWorkLocation.lng;
-
-          }
         }
         if(this.Contact){
           toReturn.contactStatus = this.Contact.status;
