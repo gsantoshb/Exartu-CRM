1285530eced0603744bb0921ead227c9fccc5681
javiber <javier.berneche@gmail.com>
2015-08-06 14:21:47 -0300

    some changes to esCollection

--- common/esCollections.js
+++ common/esCollections.js
@@ -3,15 +3,17 @@ ES.syncCollection({
 	collection: Contactables,
 	fields: [
 		{ name: 'idField', label: 'Id'},
-		{ name: 'person.firstName', label: 'First name', boost:5}, { name: 'person.lastName', label: 'Last name', boost:5},
-		{ name: 'person.middleName', label: 'Middle name', boost:3}, { name: 'person.jobTitle', label: 'Job title'},
+		{ name: 'person.firstName', label: 'First name', boost:5},
+    { name: 'person.lastName', label: 'Last name', boost:5},
+		{ name: 'person.middleName', label: 'Middle name', boost:3},
+    { name: 'person.jobTitle', label: 'Job title'},
 		{ name: 'organization.organizationName', label: 'Organization name', boost:5},
 		{ name: 'Client.department', label: 'Deparment', boost:3},
-		{ name: 'Contact.statusNote', label: 'Status note'},
-		{ name: 'Employee.statusNote', label: 'Status note'},
+		{ name: 'statusNote', label: 'Status note'},
 		{ name: 'Employee.status', label: 'status'},
 		{ name: 'Employee.taxID', label: 'SSN'},
-		{ name: 'tags', label: 'tags'}, { name: 'contactMethods.value', label: 'Contact method'}, { name: 'notes.value', label: 'Notes'}
+		{ name: 'tags', label: 'tags'},
+    { name: 'contactMethods.value', label: 'Contact method'}
 	],
 	// Retrive data related to items on this collections
 	// If idFiled is defined then its a "inverse relation"
