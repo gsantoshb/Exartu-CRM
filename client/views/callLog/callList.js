97c8782a05a7a713190136089b602e09d2f35b73
javiber <javier.berneche@gmail.com>
2015-09-04 17:40:19 -0300

    add filter, search and fix duration in call list

--- client/views/callLog/callList.js
+++ client/views/callLog/callList.js
@@ -3,11 +3,43 @@ CallListController = RouteController.extend({
   layoutTemplate: 'mainLayout'
 });
 var handler = null;
-Template.callList.created = function () {
-  handler = handler || Meteor.paginatedSubscribe('callList');
-};
+var type = ReactiveVar();
+var searchString = ReactiveVar();
 
-Template.callList.rendered = function() {
+Template.callList.created = function () {
+  type.set(null);
+  searchString.set('');
+
+  this.autorun(function () {
+    var filter = {};
+
+    switch (type.get()){
+      case 'inbound':
+        filter.incoming = true;
+        filter.voiceMail = { $ne: true };
+        break;
+      case 'outbound':
+        filter.incoming = { $ne: true };
+        break;
+      case 'missed':
+        filter.voiceMail = true;
+        filter.voiceMailUrl = { $exists: false };
+        break;
+      case 'voiceMail':
+        filter.voiceMailUrl = { $exists: true };
+        break;
+    }
+
+    if (searchString.get()){
+      filter.contactableName = { $regex: '.*' + searchString.get() + '.*', $options: 'i' }
+    }
+
+    if (handler){
+      handler.setFilter(filter)
+    } else {
+      handler = Meteor.paginatedSubscribe('callList', { filter: filter});
+    }
+  });
 };
 
 Template.callList.destroyed = function () {
@@ -19,7 +51,46 @@ Template.callList.helpers({
   activities: function () {
     return Calls.find({},{sort: { dateCreated: -1 } });
   },
-  logThis: function () {
-    console.log(this);
+  callTypeOptions: function () {
+    return [{
+      displayName: 'Inbound',
+      value: 'inbound'
+    },{
+      displayName: 'Outbound',
+      value: 'outbound'
+    },{
+      displayName: 'Missed',
+      value: 'missed'
+    },{
+      displayName: 'Voice Mail',
+      value: 'voiceMail'
+    }]
+  },
+  isSelectedClass: function () {
+    if (type.get() != this.value){
+      return 'btn-default';
+    } else {
+      return 'btn-primary';
+    }
+  },
+  getDuration: function () {
+    if (this.duration >= 60){
+      return Math.ceil(this.duration / 60) + ' minutes';
+    }else {
+      return this.duration + ' seconds';
+    }
   }
+});
+
+Template.callList.events({
+  'click .type-filter': function (e, ctx) {
+    if (type.get() == this.value){
+      type.set(null);
+    }else{
+      type.set(this.value);
+    }
+  },
+  'keyup #searchString': _.debounce(function (e, ctx) {
+    searchString.set(e.target.value);
+  }, 200)
 });
